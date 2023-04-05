a3c99ea6516afef943fea33307139fcc3dfbe1bf
Olawalesegun <olawalesegun0@gmail.com>
2023-04-05 10:04:32 +0100

    Updates before the usage of switch and not if else

--- GettingHandsDirtyDoingTheWork/Calculation.java
+++ GettingHandsDirtyDoingTheWork/Calculation.java
@@ -6,6 +6,26 @@ public class Calculation {
 
     public static void main(String[] args){
         System.out.println(isEvenNumber(4));
+
+        //Print Even numbers from 1 to 20
+        int noOfTimeItLoopedForOdd = 0;
+        int noOfTimeItLoopedForEven = 0;
+        for(int i = 1; i<=21; i++){
+            if(!isEvenNumber(i)){
+                noOfTimeItLoopedForOdd++;
+                System.out.println("The number of times it looped for Odd numbers: " + noOfTimeItLoopedForOdd);
+                continue;
+            }
+            else{
+                System.out.println("Even numbers are: " + i);
+                noOfTimeItLoopedForEven++;
+                if(noOfTimeItLoopedForEven>5){
+                    System.out.println("The number of times it looped for Even numbers: " + noOfTimeItLoopedForEven);
+                    break;
+                }
+            }
+
+        }
     }
 
     public static boolean isEvenNumber(int number){
