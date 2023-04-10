890411635fde2d3272d502a2162060ebb85cf80f
Olawalesegun <olawalesegun0@gmail.com>
2023-04-10 19:48:36 +0100

    A program to confirm Prime Number

--- Snacks/PrimeNumber.java
+++ Snacks/PrimeNumber.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class PrimeNumber {
+import java.util.Scanner;
+public class PrimeNumber {
+
+
+    //Write a program that prompts the user to input a positive integer.
+    // It should then output a message indicating whether the number is
+    // a prime Number.
+    public static void main(String[] args){
+        primeNumber();
+    }
+
+    public static void primeNumber(){
+        Scanner userInput = new Scanner(System.in);
+        int primeNumberInputs;
+        int loopCounter;
+        int primeNumberCounter = 0;
+        System.out.println("Hey Mate!! Enter a positive Integer to check " +
+                "if it's a Prime Number");
+        primeNumberInputs = userInput.nextInt();
+        if(primeNumberInputs>0){
+            for(loopCounter=1; loopCounter<=primeNumberInputs; loopCounter++){
+                if(primeNumberInputs%loopCounter == 0){
+                    //System.out.println(primeNumberInputs);
+                    primeNumberCounter++;
+                }
+            }
+            if(primeNumberCounter==2){
+                System.out.println(primeNumberInputs + " is a Prime Number");
+            }
+            else{
+                System.out.println(primeNumberInputs + " is not a Prime Number");
+            }
+        }
+        else{
+            System.out.println("Hey Mate!! You have not entered a positive number.");
+        }
+    }
 }
