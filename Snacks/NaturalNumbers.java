8baac542a13d475f5de62e09a4dfcebd8e5ed7eb
Olawalesegun <olawalesegun0@gmail.com>
2023-04-10 21:12:23 +0100

    Refactoring natural number to ensure it takes the first 10 and not any other number

--- Snacks/NaturalNumbers.java
+++ Snacks/NaturalNumbers.java
@@ -5,11 +5,19 @@ public class NaturalNumbers {
         //Write a program to print the sum of first 10 natural number.
         Scanner userInput = new Scanner(System.in);
         int total = 0;
-        System.out.println("Kindly Enter a first Natural number");
+        System.out.println("Kindly Enter 1 as a first Natural number");
         int naturalNumber = userInput.nextInt();
-        for(;naturalNumber<=10; naturalNumber++){
-            total += naturalNumber;
+        if(naturalNumber==1 ){
+            for(;naturalNumber<=10; naturalNumber++){
+                total += naturalNumber;
+            }
+            System.out.println("The total is: "+ total);
         }
-        System.out.println("The total is: "+ total);
+        else{
+            System.out.println("Hey, My Friend!! " +
+                    "To print the first 10 Natural number" +
+                    " Kindly enter 1");
+        }
+
     }
 }
