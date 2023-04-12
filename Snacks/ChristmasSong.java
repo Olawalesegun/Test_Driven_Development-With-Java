47c11a6be3d36f46c25fb4f50f1fef6839908097
Olawalesegun <olawalesegun0@gmail.com>
2023-04-12 01:59:18 +0100

    updates on Christmas ong

--- Snacks/ChristmasSong.java
+++ Snacks/ChristmasSong.java
@@ -5,148 +5,35 @@ public class ChristmasSong{
         int conditionUsed = 12;
         for(iteratingValue = 1; iteratingValue<=conditionUsed; iteratingValue++){
             switch(iteratingValue){
-                case 1 ->
-                    System.out.println("""
-                            On the first day of Christmas
-                            my true love sent to me:
-                            A Partridge in a Pear Tree
-                            """);
-                case 2 ->
-                    System.out.println("""
-                            On the second day of Christmas
-                            my true love sent to me:
-                            2 Turtle Doves
-                            and a Partridge in a Pear Tree
-                            """);
-
-                case 3 ->
-                    System.out.println("""
-                            On the third day of Christmas
-                            my true love sent to me:
-                            3 French Hens
-                            2 Turtle Doves
-                            and a Partridge in a Pear Tree
-                            """);
-                case 4 ->
-                    System.out.println("""
-                            On the fourth day of Christmas
-                            my true love sent to me:
-                            4 Calling Birds
-                            3 French Hens
-                            2 Turtle Doves
-                            and a Partridge in a Pear Tree
-                            """);
-                case 5 ->
-                    System.out.println("""
-                            On the fifth day of Christmas
-                            my true love sent to me:
-                            5 Golden Rings
-                            4 Calling Birds
-                            3 French Hens
-                            2 Turtle Doves
-                            and a Partridge in a Pear Tree
-                            """);
-                case 6 ->
-                    System.out.println("""
-                            On the sixth day of Christmas
-                            my true love sent to me:
-                            6 Geese a Laying
-                            5 Golden Rings
-                            4 Calling Birds
-                            3 French Hens
-                            2 Turtle Doves
-                            and a Partridge in a Pear Tree
-                            """);
-                case 7 ->
-                    System.out.println("""
-                            On the seventh day of Christmas
-                            my true love sent to me:
-                            7 Swans a Swimming
-                            6 Geese a Laying
-                            5 Golden Rings
-                            4 Calling Birds
-                            3 French Hens
-                            2 Turtle Doves
-                            and a Partridge in a Pear Tree
-                            """);
-                case 8 ->
-                    System.out.println("""
-                            On the eighth day of Christmas
-                            my true love sent to me:
-                            8 Maids a Milking
-                            7 Swans a Swimming
-                            6 Geese a Laying
-                            5 Golden Rings
-                            4 Calling Birds
-                            3 French Hens
-                            2 Turtle Doves
-                            and a Partridge in a Pear Tree
-                            """);
-                case 9 ->
-                    System.out.println("""
-                            On the ninth day of Christmas
-                            my true love sent to me:
-                            9 Ladies Dancing
-                            8 Maids a Milking
-                            7 Swans a Swimming
-                            6 Geese a Laying
-                            5 Golden Rings
-                            4 Calling Birds
-                            3 French Hens
-                            2 Turtle Doves
-                            and a Partridge in a Pear Tree
-                            """);
-                case 10 ->
-                    System.out.println("""
-                            On the tenth day of Christmas
-                            my true love sent to me:
-                            10 Lords a Leaping
-                            9 Ladies Dancing
-                            8 Maids a Milking
-                            7 Swans a Swimming
-                            6 Geese a Laying
-                            5 Golden Rings
-                            4 Calling Birds
-                            3 French Hens
-                            2 Turtle Doves
-                            and a Partridge in a Pear Tree
-                            """);
-                case 11 ->
-                    System.out.println("""
-                            On the eleventh day of Christmas
-                            my true love sent to me:
-                            11 Pipers Piping
-                            10 Lords a Leaping
-                            9 Ladies Dancing
-                            8 Maids a Milking
-                            7 Swans a Swimming
-                            6 Geese a Laying
-                            5 Golden Rings
-                            4 Calling Birds
-                            3 French Hens
-                            2 Turtle Doves
-                            and a Partridge in a Pear Tree
-                            """);
-                case 12 ->
-                    System.out.println("""
-                            On the twelfth day of Christmas
-                            my true love sent to me:
-                            12 Drummers Drumming
-                            11 Pipers Piping
-                            10 Lords a Leaping
-                            9 Ladies Dancing
-                            8 Maids a Milking
-                            7 Swans a Swimming
-                            6 Geese a Laying
-                            5 Golden Rings
-                            4 Calling Birds
-                            3 French Hens
-                            2 Turtle Doves
-                            and a Partridge in a Pear Tree
-                            """);
+                case 1 -> System.out.println("On the first day of Christmas my true love sent to me");
+                case 2 -> System.out.println("On the second day of Christmas my true love sent to me");
+                case 3 -> System.out.println("On the third day of Christmas my true love sent to me");
+                case 4 -> System.out.println("On the fourth day of Christmas my true love sent to me");
+                case 5 -> System.out.println("On the fifth day of Christmas my true love sent to me");
+                case 6 -> System.out.println("On the sixth day of Christmas my true love sent to me");
+                case 7 -> System.out.println("On the seventh day of Christmas my true love sent to me");
+                case 8 -> System.out.println("On the eighth day of Christmas my true love sent to me");
+                case 9 -> System.out.println("On the ninth day of Christmas my true love sent to me");
+                case 10 -> System.out.println("On the tenth day of Christmas my true love sent to me");
+                case 11 -> System.out.println("On the eleventh day of Christmas my true love sent to me");
+                case 12 -> System.out.println("On the twelfth day of Christmas my true love sent to me");
+            }
+            for(;iteratingValue<=conditionUsed;)
+            switch(iteratingValue){
+                case 12: System.out.println("12 Drummers Drumming");
+                case 11: System.out.println("11 Pipers Piping");
+                case 10: System.out.println("10 Pipers Piping");
+                case 9: System.out.println("9 Pipers Piping");
+                case 8: System.out.println("8 Pipers Piping");
+                case 7: System.out.println("7 Pipers Piping");
+                case 6: System.out.println(" Pipers Piping");
+                case 5: System.out.println("5 Pipers Piping");
+                case 4: System.out.println("4 Pipers Piping");
+                case 3: System.out.println("3 Pipers Piping");
+                case 2: System.out.println("2 Pipers Piping");
+                case 1: System.out.println("1 Pipers Piping");
             }
         }
     }
-
 }
 
