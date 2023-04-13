660d5eca4d40442984d902c14556427c7fdeeeb1
Olawalesegun <olawalesegun0@gmail.com>
2023-04-14 00:41:37 +0100

    in here I wrote a program that checks for the highest and lowest numbers entered by a user in a sentinel controlled iteration

--- Snacks/GreaterLower.java
+++ Snacks/GreaterLower.java
@@ -1,2 +1,34 @@
+import java.util.Scanner;
+
 public class GreaterLower {
+
+    public static void main(String... args) {
+        greaterAndLower();
+    }
+
+    public static int greaterAndLower() {
+        int swapNo = 0;
+        int greaterValue = Integer.MIN_VALUE;
+        int lowerValue = Integer.MAX_VALUE;
+        int num = 1;
+        int numberToCompare;
+        Scanner userInput = new Scanner(System.in);
+        while (num != 0) {
+            System.out.println("Enter a number that to check for which is greater or lower");
+            numberToCompare = userInput.nextInt();
+            //swapNo = numberToCompare;
+            if (numberToCompare < lowerValue) {
+                lowerValue = numberToCompare;
+            }
+            if (numberToCompare > greaterValue) {
+                greaterValue = numberToCompare;
+            }
+            System.out.printf("The greater number you entered is: %d%n", greaterValue );
+            System.out.printf("The lower number you entered is: %d%n", lowerValue);
+
+            System.out.println("To halt this loop, enter 0, while to continue Press a number other than 0");
+            num = userInput.nextInt();
+        }
+        return swapNo;
+    }
 }
