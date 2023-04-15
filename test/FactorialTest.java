eae940bd954c5e9503489362fd6aa8e23db202e0
Olawalesegun <olawalesegun0@gmail.com>
2023-04-15 16:54:07 +0100

    test case for Factorial

--- test/FactorialTest.java
+++ test/FactorialTest.java
@@ -1,4 +1,12 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class FactorialTest {
-  
+    @Test
+    public void factorialCheck(){
+        Factorial fac = new Factorial();
+        int facReturn = fac.factorialInput(7);
+        assertEquals(5040, facReturn);
+    }
 }
