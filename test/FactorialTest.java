86e9f6a9cc92bae5df02c244590d34d6e977e0c4
Olawalesegun <olawalesegun0@gmail.com>
2023-04-15 19:31:17 +0100

    refactoring the test case for Factorial and testing for edga cases

--- test/FactorialTest.java
+++ test/FactorialTest.java
@@ -3,23 +3,21 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FactorialTest {
+    Factorial fac = new Factorial();
     @Test
     public void factorialCheck(){
-        Factorial fac = new Factorial();
         int facReturn = fac.factorialInput(7);
         assertEquals(5040, facReturn);
     }
 
     @Test
     public void factorialCheck2(){
-        Factorial fac = new Factorial();
         int facReturn = fac.factorialInput(-5);
         assertEquals(0, facReturn);
     }
 
     @Test
     public void factorialCheck3(){
-        Factorial fac = new Factorial();
         int facReturn = fac.factorialInput(34);
         assertEquals(0, facReturn);
     }
@@ -27,21 +25,18 @@ class FactorialTest {
     //SECOND APPROACH TO FACTORIAL
     @Test
     public void factorialCheck2ndApproach(){
-        Factorial fac = new Factorial();
         int facReturn = fac.factorialSecondApproach(7);
         assertEquals(5040, facReturn);
     }
 
     @Test
     public void factorialCheck2ndApproach2(){
-        Factorial fac = new Factorial();
         int facReturn = fac.factorialSecondApproach(34);
         assertEquals(0,facReturn);
     }
 
     @Test
     public void factorialCheck2ndApproach3(){
-        Factorial fac = new Factorial();
         int facReturn = fac.factorialSecondApproach(33);
         assertEquals(-2147483648, facReturn);
     }
