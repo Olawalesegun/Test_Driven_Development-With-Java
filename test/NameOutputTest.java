202ef028b5b34811acc11f555991a9b380d4e9a3
Olawalesegun <olawalesegun0@gmail.com>
2023-04-15 16:56:00 +0100

    a testcase for naming program in the classwork directory

--- test/NameOutputTest.java
+++ test/NameOutputTest.java
@@ -1,4 +1,14 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class NameOutputTest {
-  
+
+    @Test
+    public void nameTest(){
+        NameOutput name = new NameOutput();
+        String shigo = name.namingOutput("Segun");
+        assertEquals("Hello, Segun", shigo);
+    }
+
 }
