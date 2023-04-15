162599c79e0346eaff0d6ba8fd081a88c0961c4f
Olawalesegun <olawalesegun0@gmail.com>
2023-04-15 19:22:37 +0100

    refactoring the test case for Factorial and testing for edga cases

--- ClassWork/Factorial.java
+++ ClassWork/Factorial.java
@@ -1,8 +1,6 @@
-import java.util.stream.IntStream;
-
 public class Factorial {
 
-    public static int factorialInput(int facNumber){
+    public int factorialInput(int facNumber){
         int facResult;
             facResult = facNumber;
             while(facNumber!=1){
@@ -23,12 +21,11 @@ public class Factorial {
     // in a loop while taking out the zero option
     // because a multiplication of zeros will always equal to zero
 
-    public static int factorialSecondApproach(int facNumberDeclared){
+    public int factorialSecondApproach(int facNumberDeclared){
         int totalFacCollector = 1;
         for(int counterforFactorial = 1; counterforFactorial<=facNumberDeclared; counterforFactorial++){
             totalFacCollector = totalFacCollector * counterforFactorial;
         }
-        System.out.println(totalFacCollector);
         return totalFacCollector;
     }
  /*   int totalFacCollector = IntStream.iterate(1, counterforFactorial -> counterforFactorial <= FacNumberDeclared, counterforFactorial -> counterforFactorial + 1).reduce(1, (a, b) -> a * b);
