45aae1539c5a23bc74fb0b3db709a39b82479b8b
Olawalesegun <olawalesegun0@gmail.com>
2023-04-15 18:27:27 +0100

    checking for all possible edge cases in Factorial

--- test/FactorialTest.java
+++ test/FactorialTest.java
@@ -9,4 +9,40 @@ class FactorialTest {
         int facReturn = fac.factorialInput(7);
         assertEquals(5040, facReturn);
     }
+
+    @Test
+    public void factorialCheck2(){
+        Factorial fac = new Factorial();
+        int facReturn = fac.factorialInput(-5);
+        assertEquals(0, facReturn);
+    }
+
+    @Test
+    public void factorialCheck3(){
+        Factorial fac = new Factorial();
+        int facReturn = fac.factorialInput(34);
+        assertEquals(0, facReturn);
+    }
+
+    //SECOND APPROACH TO FACTORIAL
+    @Test
+    public void factorialCheck2ndApproach(){
+        Factorial fac = new Factorial();
+        int facReturn = fac.factorialSecondApproach(7);
+        assertEquals(5040, facReturn);
+    }
+
+    @Test
+    public void factorialCheck2ndApproach2(){
+        Factorial fac = new Factorial();
+        int facReturn = fac.factorialSecondApproach(34);
+        assertEquals(0,facReturn);
+    }
+
+    @Test
+    public void factorialCheck2ndApproach3(){
+        Factorial fac = new Factorial();
+        int facReturn = fac.factorialSecondApproach(33);
+        assertEquals(-2147483648, facReturn);
+    }
 }
