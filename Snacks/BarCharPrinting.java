97d9a38ca934a89b20b4f4e2b1fa7338aaaa76f8
Olawalesegun <olawalesegun0@gmail.com>
2023-04-16 01:59:58 +0100

    Program to print Bar Chart pattern

--- Snacks/BarCharPrinting.java
+++ Snacks/BarCharPrinting.java
@@ -1,2 +1,50 @@
+import java.util.Scanner;
 public class BarCharPrinting {
+
+    public static void main(String[] args) {
+        barChartprint();
+    }
+    //read 5 numbers
+    //between 1 and 30
+    // for each number read, display number of asterisk
+
+    public static void barChartprint(){
+        Scanner userInput = new Scanner(System.in);
+        int numberArray[] = new int[0];
+        int count =0;
+        int loopKill = 1;
+        while(loopKill != 0) {
+            System.out.println("Enter 5 numbers: ");
+            numberArray[count] = userInput.nextInt();
+            count++;
+        }
+        if(count==1){
+            for(int j=1; j<numberArray[count]; j++){
+                System.out.print("*");
+            }
+        }
+        if(count==2){
+            for(int j = 1; j<=numberArray[count]; j++){
+                System.out.print("*");
+            }
+        }
+        if(count==3){
+            for(int j=1; j<=numberArray[count]; j++){
+                System.out.print("*");
+            }
+        }
+        if(count==4){
+            for(int j=1; j<=numberArray[count]; j++){
+                System.out.print("*");
+            }
+        }
+        if(count==5){
+            for(int j=1; j<=numberArray[count]; j++){
+                System.out.print("*");
+            }
+        }
+        System.out.println("To halt this, enter 0");
+        loopKill = userInput.nextInt();
+    }
+    }
 }
