557b57de09d99bc918b0fa6f7412c660b8e603c2
Olawalesegun <olawalesegun0@gmail.com>
2023-04-16 01:54:35 +0100

    A program to reverse the Multiplication table

--- Snacks/ReverseMultiplication.java
+++ Snacks/ReverseMultiplication.java
@@ -1,2 +1,12 @@
 public class ReverseMultiplication {
+
+    public int reverseMultiply(int number){
+        int total = 1;
+        for(int counter = 12; counter>=0; counter--){
+            total = number * counter;
+            //System.out.println(number + " * " + counter + " = " + total);
+        }
+        return total;
+
+    }
 }
