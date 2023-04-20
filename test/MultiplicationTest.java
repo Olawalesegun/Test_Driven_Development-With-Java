3773d8996796d0edc1737aac9bfbbb1d88d11d60
Olawalesegun <olawalesegun0@gmail.com>
2023-04-20 23:04:30 +0100

    Updates on multiplication test

--- test/MultiplicationTest.java
+++ test/MultiplicationTest.java
@@ -1,4 +1,23 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MultiplicationTest {
-  
+    Multiplication multiply = new Multiplication();
+    @Test
+    public void multiplyTest(){
+    int mul = multiply.multiplyTable(2);
+    assertEquals(24, mul);
+    }
+
+    @Test
+    public void multiplyTest2(){
+        assertEquals(60, multiply.multiplyTable(5));
+    }
+
+    @Test
+    public void multiplyTest3(){
+        int mul = multiply.multiplyTable(-4);
+        assertEquals(0, mul);
+    }
 }
