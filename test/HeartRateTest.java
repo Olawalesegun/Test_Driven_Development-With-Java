3840201df875d78df1cec452750f2d8decd1daa0
Olawalesegun <olawalesegun0@gmail.com>
2023-04-20 23:02:30 +0100

    the test for Heart Rate program

--- test/HeartRateTest.java
+++ test/HeartRateTest.java
@@ -1,4 +1,16 @@
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
 class HeartRateTest {
-  
+
+    @Test
+    public void constructorTest(){
+        HeartRate heart = new HeartRate("Segun", "Olawale", "56-76-1990");
+        assertNotNull(heart);
+    }
+     @Test
+    public void getFirstnameTest(){
+
+     }
 }
