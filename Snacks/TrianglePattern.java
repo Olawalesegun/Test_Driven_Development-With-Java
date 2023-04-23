c9d3b76dbd954596898cf397f9486e644c633b4f
Olawalesegun <olawalesegun0@gmail.com>
2023-04-23 22:00:37 +0100

    Updates on Triangle printing

--- Snacks/TrianglePattern.java
+++ Snacks/TrianglePattern.java
@@ -28,15 +28,27 @@ public class TrianglePattern {
     }
 
     public static void upsideRightTrianglePattern(){
-        int condition = 10;
+        int conditionForLoop = 10;
+        for(int loop1=1; loop1<=conditionForLoop; loop1++){
+            for(int loop2=conditionForLoop; loop2<=loop1 ; loop2--){
+                System.out.print("*");
+            }
+            System.out.println();
+            for(int loop3=0; loop3<loop1; loop3++){
+                System.out.print(" ");
+            }
+        }
+        /*int condition = 10;
         int loop2;
         for(int loopCount=1; loopCount<=condition; loopCount++){
             for(loop2=loopCount; loop2<=condition; loop2++){
                 System.out.print("*");
             }
             System.out.println();
+            for(int loop3 = 0; loop3<loopCount; loopCount++){
+                System.out.println(" ");
+            }*/
          //   for(int i= 1; i<condition; i++){
           //     for(j = 1; ) System.out.print(" ");
-            }
-        }
     }
+}
