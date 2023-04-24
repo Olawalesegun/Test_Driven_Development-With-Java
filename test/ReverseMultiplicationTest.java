8ef73450d4f315408ce60644ae93a21873f43454
Olawalesegun <olawalesegun0@gmail.com>
2023-04-24 08:09:19 +0100

    Test written to the test the edge cases for the reversemul method

--- test/ReverseMultiplicationTest.java
+++ test/ReverseMultiplicationTest.java
@@ -1,4 +1,18 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ReverseMultiplicationTest {
-  
+    ReverseMultiplication reverseMul = new ReverseMultiplication();
+    @Test
+    public void reverseMultiplyTest(){
+        int rev = reverseMul.reverseMultiply(10);
+        assertEquals(0, rev);
+    }
+
+    @Test
+    public void reverseMultiplyTest2(){
+       assertEquals(0, reverseMul.reverseMultiply(7));
+    }
+
 }
