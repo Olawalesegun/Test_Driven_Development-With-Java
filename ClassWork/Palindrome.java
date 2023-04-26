c67f06a53ba6086061109e65bc539ab32d4718df
Olawalesegun <olawalesegun0@gmail.com>
2023-04-26 23:38:56 +0100

    first to many attempt on palindrome

--- ClassWork/Palindrome.java
+++ ClassWork/Palindrome.java
@@ -1,2 +1,23 @@
 public class Palindrome {
+    public String palindromeCheck(String string) {
+        int requirement = 5;
+        String newChar = null;
+        String newChar2 = "";
+        if (string.length() == requirement) {
+            for (int count = 0; count < string.length(); count++) {
+                newChar += string.charAt(count);
+            }
+            for (int count = 4; count >= 0; count--) {
+                newChar2 += newChar.charAt(count);
+            }
+            if (newChar2.equals(newChar)) {
+                System.out.println("The string entered is a palindrome");
+            } else {
+                System.out.println("The string entered is not a palindrome");
+            }
+        } else {
+            System.out.println("My Dear Brother stop playing and get serious with life");
+        }
+        return newChar2;
+    }
 }
