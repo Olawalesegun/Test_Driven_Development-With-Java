f84baeccedfbb9db2b01c8964387f87d2473cfd1
Olawalesegun <olawalesegun0@gmail.com>
2023-04-27 20:34:14 +0100

    A program to print name

--- ClassWork/NameOutput.java
+++ ClassWork/NameOutput.java
@@ -1,10 +1,7 @@
 public class NameOutput {
 
     public String namingOutput(String name){
-        //System.out.println("Kindly Enter a name");
-       // String output = "Hello" + name;
         String seg = "Hello, " + name;
-
         return seg;
     }
 }
