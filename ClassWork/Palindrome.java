3d7a4b8366ef51592feed5013648f72b5f5cdcf8
Olawalesegun <olawalesegun0@gmail.com>
2023-04-27 20:26:16 +0100

    A palindrome program

--- ClassWork/Palindrome.java
+++ ClassWork/Palindrome.java
@@ -1,13 +1,13 @@
 public class Palindrome {
     public String palindromeCheck(String string) {
         int requirement = 5;
-        String newChar = null;
+        String newChar = "";
         String newChar2 = "";
         if (string.length() == requirement) {
             for (int count = 0; count < string.length(); count++) {
                 newChar += string.charAt(count);
             }
-            for (int count = 4; count >= 0; count--) {
+            for (int count = 4/*string.length() -1*/; count >= 0; count--) {
                 newChar2 += newChar.charAt(count);
             }
             if (newChar2.equals(newChar)) {
