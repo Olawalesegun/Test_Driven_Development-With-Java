51d89de2911fe47f987d00953ecbd7431e8198ec
Olawalesegun <olawalesegun0@gmail.com>
2023-04-28 17:26:12 +0100

    A program to test palindrome

--- test/PalindromeTest.java
+++ test/PalindromeTest.java
@@ -1,4 +1,13 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PalindromeTest {
-  
+    Palindrome palin = new Palindrome();
+
+    @Test
+    public void palinTest() {
+        String firstCheck = palin.palindromeCheck("31113");
+        assertEquals("31113", firstCheck);
+    }
 }
