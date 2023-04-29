9650064f0d4c5310e89febf2c8e934ba564d6a54
Olawalesegun <olawalesegun0@gmail.com>
2023-04-29 20:51:33 +0100

    a program to test the Min Max program

--- test/MinMaxTest.java
+++ test/MinMaxTest.java
@@ -1,5 +1,15 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class MinMaxTest {
 
+class MinMaxTest {
+    MinMaxf minMax = new MinMaxf();
+    /*@Test
+    public void minMaxCalc(){
+        minMax.calcMinMax();
+    }*/
 }
+
+
+
