58798569c50663f0eaeaf2995c8ae57374ef6815
Olawalesegun <olawalesegun0@gmail.com>
2023-04-30 01:21:27 +0100

    A test case for the Play source file, this form of testing could be categorized as White Board testing

--- test/PlayTest.java
+++ test/PlayTest.java
@@ -1,5 +1,30 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayTest {
+
+    int[] args = new int [4];
+    @Test
+    public void testCheck2LargestNumber(){
+        int[] number = {2, 3, -1, 7, 8, 9};
+        String num = chek2LargestNumber(number);
+        assertEquals("98", num);
+    }
+
+    public String chek2LargestNumber(int[] numbersArray){
+        int largest = 0;
+        int secondLargest = 0;
+        for(int count: numbersArray){
+            if(count>largest){
+             //   secondLargest = largest;
+                largest = count;}
+
+        else if(count > secondLargest && count < largest){
+                secondLargest = count;
+            }
+        }
+        return largest + "" + secondLargest;
+    }
 
 }
