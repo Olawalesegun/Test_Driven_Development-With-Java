8016bca467b97885810d879b408203bc23f14c66
Olawalesegun <olawalesegun0@gmail.com>
2023-05-02 23:05:58 +0100

    this program determines the kargest number amongst a group of numbers entered

--- Chapter4/LargestNumber.java
+++ Chapter4/LargestNumber.java
@@ -9,22 +9,27 @@ public class LargestNumber {
     input and to determine when all 10 numbers have been processed).
     b) number: The integer most recently input by the user.
     c) largest: The largest number found so far.*/
+
+    public static void main(String[] args) {
+        LargestNumber largN = new LargestNumber();
+        largN.findLargestNumber();
+    }
     public int findLargestNumber() {
         Scanner userInput = new Scanner(System.in);
         int numberOfTimesToKeepEnteringNumber = 10;
         int number;
-        int largest = 0;
-        int min = 0;
-        for (int count = 0; count <= numberOfTimesToKeepEnteringNumber; count++) {
+        int largest = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int count = 0; count < numberOfTimesToKeepEnteringNumber; count++) {
             System.out.println("Kindly Enter a number: ");
             number = userInput.nextInt();
             if (number > largest) {
                 largest = number;
-            } else if (number < largest) {
+            } else if (number < min) {
                 min = number;
             }
         }
-        System.out.println("The minimum value is " + min);
-        return min;
+        System.out.println("The minimum value is " + largest);
+        return largest;
     }
 }
