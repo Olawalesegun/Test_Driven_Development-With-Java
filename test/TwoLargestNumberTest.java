685ee8f81164260ed1122daf916cd87a58a32bc5
Olawalesegun <olawalesegun0@gmail.com>
2023-05-02 22:51:39 +0100

    a test case to confirm if the Largest Number source file has a constructor present

--- test/TwoLargestNumberTest.java
+++ test/TwoLargestNumberTest.java
@@ -1,4 +1,19 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class TwoLargestNumberTest {
-  
+   /*   4.23 (Find the Two Largest Numbers) Using an approach similar to that for Exercise 4.21, find
+    the two largest values of the 10 values entered. [Note: You may input each number only once.]*/
+
+    //  TODO:
+    //  1.
+
+    TwoLargestNumber twoLN = new TwoLargestNumber();
+    @Test
+    public void testConstructor(){
+        assertNotNull(twoLN);
+    }
+
+
 }
