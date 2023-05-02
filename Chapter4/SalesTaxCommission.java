e7ac19276cb47d881360988a52c5456b85ff79a8
Olawalesegun <olawalesegun0@gmail.com>
2023-05-02 23:16:17 +0100

    update - This program checks for the SalesTaxCommision and also the current earning for an Sales person

--- Chapter4/SalesTaxCommission.java
+++ Chapter4/SalesTaxCommission.java
@@ -1,6 +1,5 @@
 public class SalesTaxCommission {
 
-
    /* 4.19 (Sales Commission Calculator) A large company pays its salespeople on a commission basis.
     The salespeople receive $200 per week plus 9% of their gross sales for that week. For example, a
     salesperson who sells $5,000 worth of merchandise in a week receives $200 plus 9% of $5,000, or
