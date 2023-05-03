aa8f987c76e9c727d508c7eb14460306ab803f00
Olawalesegun <olawalesegun0@gmail.com>
2023-05-03 01:46:24 +0100

    REMINDER ---> third trial with guidance around, not proud of it.. But I will come back to all...

--- Chapter5/ValueOfPie3.java
+++ Chapter5/ValueOfPie3.java
@@ -1,2 +1,26 @@
 public class ValueOfPie3 {
+    public static void main(String[] args) {
+        double pi = 4.0;
+        double denominator = 3.0;
+        boolean positive = false;
+        int terms = 200000;
+        int count = 0;
+        System.out.println("Terms\tValue of π");
+        for (int i = 1; i <= terms; i++) {
+            if (positive) {
+                pi += 4.0/denominator;
+            } else {
+                pi -= 4.0/denominator;
+            }
+            denominator += 2.0;
+            positive = !positive;
+            if (String.format("%.5f", pi).startsWith("3.14159")) {
+                count = i;
+            }
+            if (i % 1000 == 0) {
+                System.out.println(i + "\t" + pi);
+            }
+        }
+        System.out.println("Terms needed for first value starting with 3.14159: " + count);
+    }
 }
