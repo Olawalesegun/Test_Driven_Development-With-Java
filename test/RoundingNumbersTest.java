1773e1ed90b06b72c90cc8411bc5093d93bbcc53
Olawalesegun <olawalesegun0@gmail.com>
2023-05-04 17:12:26 +0100

    A testcase to check for edge cases in the Rounding numbers program

--- test/RoundingNumbersTest.java
+++ test/RoundingNumbersTest.java
@@ -4,18 +4,18 @@ import static org.junit.jupiter.api.Assertions.*;
 class RoundingNumbersTest {
     @Test
     public void testRoundingNumberDownForPositiveValue1(){
-       double result1 = RoundingNumbers.roundDown(9.1);
-       assertEquals(9, result1);
+       double result1 = RoundingNumbers.roundDown(9.1, 8.5);
+       assertEquals(17, result1);
     }
     @Test
     public void testRoundingNumberDownForNegativeValue(){
-        double result2 = RoundingNumbers.roundDown(-8.7);
-        assertEquals(-9, result2);
+        double result2 = RoundingNumbers.roundDown(-8.7, 2.5);
+        assertEquals(-7, result2);
     }
     @Test
     public void testRoundingNumberDownForLargeNumbers(){
-        double result3 = RoundingNumbers.roundDown(785.09);
-        assertEquals(785, result3);
+        double result3 = RoundingNumbers.roundDown(785.09, 8);
+        assertEquals(793, result3);
     }
 
 }
