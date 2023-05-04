cee49795ce99e9195bc3d752b5e170e9e10306fd
Olawalesegun <olawalesegun0@gmail.com>
2023-05-04 16:25:46 +0100

    A testcase to check for edge cases in the SalesTaxCommisionTest program

--- test/SalesTaxCommissionTest.java
+++ test/SalesTaxCommissionTest.java
@@ -1,4 +1,18 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class SalesTaxCommissionTest {
-  
+    SalesTaxCommission salesTax = new SalesTaxCommission();
+    @Test
+    public void testTaxCommission(){
+        SalesTaxCommission returnedValue = salesTax.getWeeklySales(6500).getTaxCommission().getWeeklyEarnings();
+        assertEquals(785, returnedValue);
+    }
+    @Test
+    public void test2TaxCommission(){
+        SalesTaxCommission returnedValue = salesTax.getWeeklySales(-5000).getTaxCommission().getWeeklyEarnings();
+        assertEquals(-250, returnedValue);
+    }
+
 }
