948c50fdce117bb7e47d056a2885b1f50e948072
Olawalesegun <olawalesegun0@gmail.com>
2023-05-06 01:40:47 +0100

    a program to build --toString method-- using Arrays and computing it into another Array

--- ClassWork/ArraySamples.java
+++ ClassWork/ArraySamples.java
@@ -1,2 +1,24 @@
+import java.util.Arrays;
+
 public class ArraySamples {
+    public static void main(String[] args) {
+        int[] array = new int[10];
+        array[2] = 20;
+        System.out.println(array);
+        System.out.println(toString(array));
+        //  using the array in built function Arrays.toString to print
+        array[2] = 30;
+        System.out.println(Arrays.toString(array));
+    }
+
+    private static String toString(int[] array){
+        String arrayRepresentation = "[";
+        for(int index=0; index<array.length; index++){
+            if(index != array.length -1) arrayRepresentation += array[index] + ",";
+            else arrayRepresentation += array[index];
+
+        }
+        arrayRepresentation += "]";
+        return arrayRepresentation;
+    }
 }
