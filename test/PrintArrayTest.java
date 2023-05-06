e91a0c420435a857db1df7138e1547e0b735ca3a
Olawalesegun <olawalesegun0@gmail.com>
2023-05-06 02:01:57 +0100

    a testcase to test that the PrintArray program meets the requirement of building a toString and the printArray prints the required result at all time any argument is passed

--- test/PrintArrayTest.java
+++ test/PrintArrayTest.java
@@ -1,4 +1,42 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PrintArrayTest {
-  
+
+   /* @Test
+    public void testArray(){
+        String arrayValues = PrintArray.determineLengthOfArray(5);
+        assertEquals(5,arrayValues);
+        //assertArrayEquals([5,4,3,2,1,0], arrayValues);
+    }*/
+    @Test
+    public void testArray(){
+        int[] array = {4, 5, 6, 7};
+        //int[] array2 = new int[]{PrintArray.printArray(array)};
+        PrintArray.printArray(array);
+        int[] array2 = {1,2,3,4};
+        assertEquals(Arrays.toString(array2), Arrays.toString(PrintArray.printArray(array)));
+    }
+
+    @Test
+    public void testArray2(){
+        int[] array = {4, 0, 6, 7, 0, 0, 0, 0, 0, 0};
+        //int[] array2 = new int[]{PrintArray.printArray(array)};
+        PrintArray.printArray(array);
+        int[] array2 = {1,2,3,4,5,6,7,8,9,10};
+        assertEquals(Arrays.toString(array2), Arrays.toString(PrintArray.printArray(array)));
+    }
+
+    @Test
+    public void insertElementsIntoArrayTest(){
+        int[] array = new int[40];
+       // int[] expected = {1,2,3,4,5,6,7,8,9,10};
+        int[] actual = PrintArray.printArray(array);
+        for(int index = 0; index < actual.length; index++){
+            assertEquals(index + 1, actual[index]);
+        }
+    }
 }
