c59dc09087f57c2fb4192c261991323323cf253b
Olawalesegun <olawalesegun0@gmail.com>
2023-05-06 01:59:06 +0100

    a program to collect random numbers that are non deterministic in an Array of 20 elements

--- GettingHandsDirtyDoingTheWork/SecRandom.java
+++ GettingHandsDirtyDoingTheWork/SecRandom.java
@@ -1,2 +1,18 @@
-public class secRandom {
+import java.security.SecureRandom;
+import java.util.Arrays;
+
+public class SecRandom {
+
+    public static void main(String[] args) {
+        SecRandom.secRan();
+    }
+    public static int[] secRan(){
+        SecureRandom  sec = new SecureRandom();
+        int[] collectorOfInts = new int[20];
+        for(int counterValue = 0; counterValue<collectorOfInts.length; counterValue++){
+            collectorOfInts[counterValue] = 2 + sec.nextInt(8);
+        }
+        System.out.println(Arrays.toString(collectorOfInts));
+        return collectorOfInts;
+    }
 }
