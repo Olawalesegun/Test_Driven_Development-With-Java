31760f761b8ee4029d2ea89bf47c692b00d55edf
Olawalesegun <olawalesegun0@gmail.com>
2023-05-07 11:34:54 +0100

    a program to calc hypothenuse while not using any inbuilt function to calc in the program

--- Chapter6/HypotenuseCalculation.java
+++ Chapter6/HypotenuseCalculation.java
@@ -5,7 +5,20 @@ public class HypotenuseCalculation {
     application that reads values for side1 and side2 and performs the calculation with the hypotenuse
     method. Use Math methods pow and sqrt to determine the length of the hypotenuse for each of the
     triangles in Fig. 6.14. [Note: Class Math also provides method hypot to perform this calculation.]*/
-    public double hypotenuse(double num1, double num2){
-
+    public static double hypotenuse(double side1, double side2){
+        double side1Squared = side1 * side1;
+        double side2Squared = side2 * side2;
+        double sumOfSides = side1Squared + side2Squared;
+        int numberOfTimesToLoop = 5;
+        double squareRootValue = 2;
+        for(int loopThroughIndexValue = 0; loopThroughIndexValue<= numberOfTimesToLoop; loopThroughIndexValue++){
+             squareRootValue = (squareRootValue + (sumOfSides/ squareRootValue))/2;
+        }
+        return squareRootValue;
+    }
+    public static String convertMyFloatingNumberTo3Precision(double number){
+        String formattedStringTo3Precision = String.format("%.3f",number);
+        System.out.println(formattedStringTo3Precision);
+        return formattedStringTo3Precision;
     }
 }
