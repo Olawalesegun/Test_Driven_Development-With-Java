cfe89b304d201a69f21efc442b52801243c41d00
Olawalesegun <olawalesegun0@gmail.com>
2023-05-09 23:25:34 +0100

    Practice on arrays in java

--- ClassWork/PrintArray2.java
+++ ClassWork/PrintArray2.java
@@ -1,2 +1,28 @@
+import java.util.Arrays;
+
 public class PrintArray2 {
+
+    public static void main(String[] args) {
+        String[] words = {"Hello", "Thank you", "Gems"};
+        System.out.println("WORDS:: " + Arrays.toString(words));
+
+       /* SemicolonNative[] natives = {
+                new SemicolonNative(name: "Sultan", cohort: 16),
+                new SemicolonNative(name: "Mariam", cohort: 16),
+                new SemicolonNative(name: "Ebuka", cohort: 16),
+                new SemicolonNative(name: "Ashley", cohort: 16),
+                new SemicolonNative(name: "Ashley", cohort: 16)
+        };*/
+
+
+        /////
+       /* SemicolonNative[] natives = new SemicolonNative[4];
+        natives[0] = new SemicolonNative(name: "Sultan", cohort: 16),
+        natives[1] = new SemicolonNative(name: "Mariam", cohort: 16),
+        natives[2] = new SemicolonNative(name: "Ebuka", cohort: 16),
+        natives[3] = new SemicolonNative(name: "Ashley", cohort: 16),
+        natives[4] = new SemicolonNative(name: "Ashley", cohort: 16)
+        System.out.println("NATIVES:: "+ natives);
+        };*/
+    }
 }
