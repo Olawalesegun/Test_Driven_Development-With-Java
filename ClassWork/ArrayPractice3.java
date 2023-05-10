904e8dcb1bcae42321334f49665859afbfd872cf
Olawalesegun <olawalesegun0@gmail.com>
2023-05-10 01:56:31 +0100

    A program to check out the largest and smallest values in an array entered/read upon declaration of trhe method

--- ClassWork/ArrayPractice3.java
+++ ClassWork/ArrayPractice3.java
@@ -1,2 +1,30 @@
 public class ArrayPractice3 {
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 5, 6, 7};
+        checkLargest(array);
+        checkSmallest(array);
+        System.out.println(checkLargest(array) + checkSmallest(array));
+    }
+    public static int checkLargest(int[] array1){
+        int largest = Integer.MIN_VALUE;
+        for (int i : array1) {
+            if (i > largest) {
+                largest = i;
+            }
+        }
+        System.out.println(largest);
+        return largest;
+    }
+
+    public static int checkSmallest(int[] array1){
+        int smallest = Integer.MAX_VALUE;
+        for (int i : array1) {
+            if (i < smallest) {
+                smallest = i;
+            }
+        }
+        System.out.println(smallest);
+        return smallest;
+    }
 }
