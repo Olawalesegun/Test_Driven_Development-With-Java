0b80d7bfabd7ffba81fefe5d2a75a083205bfcab
Olawalesegun <olawalesegun0@gmail.com>
2023-05-10 01:54:11 +0100

    a program expected to read int values from users, then add to an array and check out the total of all elemnts in array

--- ClassWork/ArrayPractice2.java
+++ ClassWork/ArrayPractice2.java
@@ -1,2 +1,30 @@
+import java.util.Scanner;
+import java.util.Arrays;
 public class ArrayPractice2 {
+
+    public static void main(String[] args) {
+        arrayPrac();
+    }
+    public static String arrayPrac(){
+        Scanner scan = new Scanner(System.in);
+        int[] arraySum1 = new int[5];
+        //int arraySum;
+        int collect;
+        int total = 0;
+        int numberCount = 5;
+        for(int count=0; count<numberCount; count++){
+            System.out.println("Enter 5 numbers: ");
+            collect = scan.nextInt();
+            arraySum1[count] = collect;
+            arraySum1[count] = arraySum1[count];
+            total += arraySum1[count];
+        }
+        System.out.println(Arrays.toString(arraySum1));
+        System.out.println("Total is: " + total);
+        return Arrays.toString(arraySum1);
+
+
+
+
+    }
 }
