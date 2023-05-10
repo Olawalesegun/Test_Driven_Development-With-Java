815ac1b5507143557695818c47f1e93e0a4faa51
Olawalesegun <olawalesegun0@gmail.com>
2023-05-10 01:58:32 +0100

    a program using Exception handling (try and catch nmethod).

--- ClassWork/ArrayExceptionHandling.java
+++ ClassWork/ArrayExceptionHandling.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class ArrayExceptionHandling {
+public class ArrayExceptionHandling {
+
+    public static void main(String[] args) {
+        tryAndCatch();
+    }
+    public static void tryAndCatch(){
+        try{
+            int[] arr = {2,5,6,7,8};
+            System.out.println(arr[10]);
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Kindly enter waht is expected o you before something do you");
+        }
+
+
+    }
 }
