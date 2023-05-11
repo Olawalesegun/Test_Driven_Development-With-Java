140baf5192d36531e856df80324e6b88ae2edb1b
Olawalesegun <olawalesegun0@gmail.com>
2023-05-11 23:30:42 +0100

    updates on test -----

--- test/PixelQuantizationTest.java
+++ test/PixelQuantizationTest.java
@@ -1,4 +1,27 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PixelQuantizationTest {
-  
+
+    PixelQuantization pixQ = new PixelQuantization();
+
+    @Test
+    public void testPixelQuant(){
+        int[] arrayName = {78, 45, 32, 59, 98, 76, 54};
+        int[] quantResult = pixQ.quantizePixel(arrayName);
+        int[] result = {70, 50, 30, 50, 90, 70, 50};
+        assertEquals(Arrays.toString(result), Arrays.toString(quantResult));
+    }
+    @Test
+    public void testPixelQuant2(){
+        int[] arrayName = {78, 45, -32, -59, 98, 76, 54};
+        int[] quantResult = pixQ.quantizePixel(arrayName);
+        int[] result = {70, 50, 30, 50, 90, 70, 50};
+        assertEquals(Arrays.toString(result), Arrays.toString(quantResult));
+    }
+
+
 }
