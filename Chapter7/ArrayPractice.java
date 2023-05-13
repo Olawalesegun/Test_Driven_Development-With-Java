5eb3257476adb976cb8a8c70ab83e2acf560e0b2
Olawalesegun <olawalesegun0@gmail.com>
2023-05-13 23:09:03 +0100

    A personal approach to TDD and ensuring to write codes Uncle Bob will be proud of

--- Chapter7/ArrayPractice.java
+++ Chapter7/ArrayPractice.java
@@ -6,26 +6,23 @@ public class ArrayPractice {
         this.arrayCollectedForUse = arrayCollectedForUse;
     }
     public int[] getArrayCollectedForUse(){
-        int[] arrayCollectedForUse1 = arrayCollectedForUse;
-        return arrayCollectedForUse1;
+        return arrayCollectedForUse;
     }
 
     public double findSumOfArray(int...arrayCollectedThroughParameter){
         arrayCollectedForUse = arrayCollectedThroughParameter;
         int[] newArrayDeclared = new int[arrayCollectedForUse.length];
-        int total = 0;
         for(int indexedNumberOfArrayForLoop1 = 0; indexedNumberOfArrayForLoop1<arrayCollectedForUse.length; indexedNumberOfArrayForLoop1++){
             newArrayDeclared[indexedNumberOfArrayForLoop1] = arrayCollectedForUse[indexedNumberOfArrayForLoop1];
         }
         for(int indexedNumberOfArrayForLoop2= 0; indexedNumberOfArrayForLoop2<newArrayDeclared.length; indexedNumberOfArrayForLoop2++){
             total += newArrayDeclared[indexedNumberOfArrayForLoop2];
         }
-        total = total;
         return total;
     }
 
-    public double getAverage(int[] array){
-        double averageGottenFromAverage = (double)total/ newArrayDeclared.length;
-        return average;
+    public double getAverage(){
+        double averageGottenFromTotal = (double)total/ arrayCollectedForUse.length;
+        return averageGottenFromTotal;
     }
 }
