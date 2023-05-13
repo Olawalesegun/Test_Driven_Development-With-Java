0fee779ccc7969eb6684da662480cb6daf18e110
Olawalesegun <olawalesegun0@gmail.com>
2023-05-13 23:46:45 +0100

    a test case to ensure that the archery game meets all requirements and no room for edge cases

--- test/ArcheryGameTest.java
+++ test/ArcheryGameTest.java
@@ -3,23 +3,33 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-/*
+
 class ArcheryGameTest {
     ArcheryGame archeryGame;
+
     @BeforeEach
     void setUp() {
         archeryGame = new ArcheryGame();
     }
 
     @Test
-    public void testArchMethod(){
+    public void testArchMethod() {
         int[][] archGame = {
-                {1,2,3,},
-                {7,8,9},
-                {7,2,1},
-                {6,4,3}
+                {1, 2, 3,},
+                {7, 8, 9},
+                {7, 2, 1},
+                {6, 4, 3}
         };
-        int result = archeryGame.playerIndex(archGame);
-        assertEquals(50, result);
+       // String result = archeryGame.playerIndex(archGame);
+        int[][] myArray = archeryGame.playerIndex(archGame);
+        assertEquals(50, myArray);
     }
-}*/
+
+    @Test
+    public void testPlayerIndex() {
+        int[][] array = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] result = archeryGame.playerIndex(array);
+        int[][] expected = {{2, 2, 3}, {4, 5, 1}, {7, 8, 5}};
+        assertArrayEquals(expected, result);
+    }
+}
