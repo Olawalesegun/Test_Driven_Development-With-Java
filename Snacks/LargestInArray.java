3f267dd9416bf219357e209843d539ca217699ef
Olawalesegun <olawalesegun0@gmail.com>
2023-05-14 19:37:57 +0100

    a program to figure out the largest number in an Array

--- Snacks/LargestInArray.java
+++ Snacks/LargestInArray.java
@@ -1,2 +1,23 @@
-public class LargestInAList {
+public class LargestInArray {
+    private int[] array;
+    private int largestNumberInArray;
+
+    public void collectArrayIntoClass(int[] array){
+        this.array = array;
+    }
+    public int[] passingArrayToACallerMethodOrMutateInstanceVariable(){
+        return array;
+    }
+    public void solveForLargestNumber(){
+        int[] arrayValuesCollectedFromAGetter = passingArrayToACallerMethodOrMutateInstanceVariable();
+        largestNumberInArray = arrayValuesCollectedFromAGetter[0];
+        for(int indexedNumberLoopingThroughArray = 0; indexedNumberLoopingThroughArray<arrayValuesCollectedFromAGetter.length; indexedNumberLoopingThroughArray++){
+            if (arrayValuesCollectedFromAGetter[indexedNumberLoopingThroughArray] > largestNumberInArray){
+                largestNumberInArray = arrayValuesCollectedFromAGetter[indexedNumberLoopingThroughArray];
+            }
+        }
+    }
+    public int collectLargestNumber(){
+        return largestNumberInArray;
+    }
 }
