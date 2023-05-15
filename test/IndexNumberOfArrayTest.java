80e13cef50048d333b59eb67df242ec6edc3268a
Olawalesegun <olawalesegun0@gmail.com>
2023-05-15 23:27:00 +0100

    a testcase to test if the IndexNumberOfArray program meets the requirement

--- test/IndexNumberOfArrayTest.java
+++ test/IndexNumberOfArrayTest.java
@@ -3,31 +3,31 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class OddNumberInArrayTest {
-    OddNumberInArray odd;
+class IndexNumberOfArrayTest {
+    IndexNumberOfArray indexed;
     @BeforeEach
     void setUp() {
-        odd = new OddNumberInArray();
+        indexed = new IndexNumberOfArray();
     }
     @Test
     public void testConstructor(){
-       assertNotNull(odd);
+       assertNotNull(indexed);
     }
     @Test
     public void testArrayToBeSureItContainsTheActualElementsEntered(){
         int[] arrayToBeUsedForTest = {5, 6, 7, 8, 13, 12, 11};
-        odd.setArrayValues(arrayToBeUsedForTest);
-        assertEquals(arrayToBeUsedForTest, odd.collectArrayValues());
+        indexed.setArrayValues(arrayToBeUsedForTest);
+        assertEquals(arrayToBeUsedForTest, indexed.collectArrayValues());
     }
     @Test
     public void testArrayInstanceVariableToReturnElementsPresentInTheIndexEntered(){
         int[] arrayToBeCollectedAndUsedForTest = {8, 7, 6, 3, 2, 1};
-        odd.setArrayValues(arrayToBeCollectedAndUsedForTest);
-        int returnedIndex = odd.returnIndexValueOFArray(2);
+        indexed.setArrayValues(arrayToBeCollectedAndUsedForTest);
+        int returnedIndex = indexed.returnIndexValueOFArray(2);
         assertEquals(6, returnedIndex);
     }
 
-    Write a function that prints the elements on odd positions in a list.
+   // Write a method that prints the elements on indexed positions in a.
     /*@Test
     public void test*/
 
