65a69ee256e37308b1a685cda3fe44e90636d812
Olawalesegun <olawalesegun0@gmail.com>
2023-05-15 23:22:46 +0100

    a program that reverses an Array

--- Snacks/Rev.java
+++ Snacks/Rev.java
@@ -1,2 +1,26 @@
 public class Rev {
+    private int[] array = new int[5];
+    private int[] newReversedArray = new int[5];
+
+    public void setArray(int[] array){
+        for(int index = 0; index<array.length; index++) {
+            this.array[index] = array[index];
+        }
+    }
+    public int[] collectArrayIntoAMethodOrMutateItsState(){
+        return array;
+    }
+
+    public void reverseArray(){
+        int[] newArray = collectArrayIntoAMethodOrMutateItsState();
+        int arraySize = newArray.length;
+        int arraySizeForIndex = arraySize - 1;
+        int indexForNewArray = 0;
+        for(int indexInArray = arraySizeForIndex; indexInArray >= 0; indexInArray--){
+            newReversedArray[indexForNewArray] = newArray[indexInArray];
+        }
+    }
+    public int[] collectReverseArray(){
+        return newReversedArray;
+    }
 }
