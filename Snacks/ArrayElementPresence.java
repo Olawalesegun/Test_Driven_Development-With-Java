a92fd91c63ee196f37f14c39b374728f02499a18
Olawalesegun <olawalesegun0@gmail.com>
2023-05-15 02:22:11 +0100

    A program to check if a number is present in an array

--- Snacks/ArrayElementPresence.java
+++ Snacks/ArrayElementPresence.java
@@ -1,6 +1,29 @@
-public class checkPresence {
+public class ArrayElementPresence {
     private int[] array;
-    public void checkElementInArray(){
+    String statusOfElementWhenChecked;
 
+
+
+    public void collectArray(int[] arrayUsedForTesting) {
+        array = arrayUsedForTesting;
+    }
+
+    public int[] mutateArrayStateWithArrayCollected() {
+        return array;
+    }
+    public void checkElementInArray(int elementToCheckFor){
+        int[] arrayCollectedAndUsed = mutateArrayStateWithArrayCollected();
+        for(int indexedThroughArrayCollectedAndUsed = 0; indexedThroughArrayCollectedAndUsed<arrayCollectedAndUsed.length; indexedThroughArrayCollectedAndUsed++){
+            if(elementToCheckFor == arrayCollectedAndUsed[indexedThroughArrayCollectedAndUsed]){
+                System.out.println(elementToCheckFor + " is present in Array");
+                statusOfElementWhenChecked = "Element Entered is present";
+            }
+            else{
+                statusOfElementWhenChecked = "Element Entered is not present (Hence, absent!!!)";
+            }
+        }
+    }
+    public String returnStatusForElementEnteredWhenCheckedInArray(){
+        return statusOfElementWhenChecked;
     }
 }
