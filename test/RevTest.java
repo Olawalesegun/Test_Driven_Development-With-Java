c2b9416df3dfbb56b74e75d05bb8ac21c6352344
Olawalesegun <olawalesegun0@gmail.com>
2023-05-16 03:14:18 +0100

    ------rev----- a testcase to test for reverse programme

--- test/RevTest.java
+++ test/RevTest.java
@@ -8,6 +8,7 @@ public class RevTest {
         Rev reverseArray = new Rev();
         int[] originalArray = {1, 2, 3, 4, 5};
         reverseArray.setArray(originalArray);
+        reverseArray.collectArrayIntoAMethodOrMutateItsState();
         reverseArray.reverseArray();
         int[] actualReversedArray = reverseArray.collectReverseArray();
         int[] expectedReversedArray = {5, 4, 3, 2, 1};
