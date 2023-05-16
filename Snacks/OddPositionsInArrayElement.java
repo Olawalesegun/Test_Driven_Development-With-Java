684fad0076193f2f0ba19e18884ee8794fad46de
Olawalesegun <olawalesegun0@gmail.com>
2023-05-16 01:46:55 +0100

    a program to print the odd numbers in an array

--- Snacks/OddPositionsInArrayElement.java
+++ Snacks/OddPositionsInArrayElement.java
@@ -1,2 +1,28 @@
+import java.util.Arrays;
+
 public class OddPositionsInArrayElement {
+
+    private int[] array = new int[6];
+
+    private int[] elementAtOddIndex = new int[array.length];
+    public void setArray(int[] arrayCollected) {
+        array = arrayCollected;
+    }
+    public int[] collectArray(){
+        return array;
+    }
+
+    public void extractElementsAtIndex() {
+        for(int indexThroughArray = 0; indexThroughArray<collectArray().length; indexThroughArray++){
+            if(indexThroughArray % 2 != 0){
+                System.out.println(collectArray()[indexThroughArray]);
+                elementAtOddIndex[indexThroughArray] = collectArray()[indexThroughArray];
+            }
+        }
+    }
+
+    public String returnExtractOddElementOutOfArray() {
+        return Arrays.toString(elementAtOddIndex);
+    }
+
 }
