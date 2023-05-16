f11f58051abea8158bb31b05e84e3b476b9f3792
Olawalesegun <olawalesegun0@gmail.com>
2023-05-16 01:42:19 +0100

    2 ---- a testcase to ensure our OddPositionArrayElement meets the requirement

--- test/OddPositionsInArrayElementTest.java
+++ test/OddPositionsInArrayElementTest.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.*;
 
 class OddPositionsInArrayElementTest {
-    //Write a function that prints the elements on odd positions in a list.
+    //Write a method that prints the elements on odd positions in a list.
     OddPositionsInArrayElement odd = new OddPositionsInArrayElement();
     @Test
     public void testOdd(){
@@ -15,6 +15,16 @@ class OddPositionsInArrayElementTest {
         odd.extractElementsAtIndex();
         String oddElementsExtractedFromArray = odd.returnExtractOddElementOutOfArray();
         String oddsAlone = "[0, 43, 0, 33, 0, 67]";
+        assertEquals(oddsAlone, oddElementsExtractedFromArray);
+    }
+    @Test
+    public void testOdd2(){
+        int[] arrayCollected = {5, 23, 14, 2, 6, 12};
+        odd.setArray(arrayCollected);
+        //int[] newArr = odd.collectArray();
+        odd.extractElementsAtIndex();
+        String oddElementsExtractedFromArray = odd.returnExtractOddElementOutOfArray();
+        String oddsAlone = "[5, 23, 0, 0, 0, 0]";
         assertEquals(oddsAlone, oddElementsExtractedFromArray);
     }
 }
