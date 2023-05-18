4a5af2f83735d59dd7f11a62d53609a255e43fe8
Olawalesegun <olawalesegun0@gmail.com>
2023-05-18 23:53:46 +0100

    reveresed array

--- Snacks/Rev.java
+++ Snacks/Rev.java
@@ -2,10 +2,19 @@ public class Rev {
     private int[] array = new int[5];
     private int[] newReversedArray = new int[5];
 
+    public static void main(String[] args) {
+        Rev rev = new Rev();
+        int[] originalArray = {1, 2, 3, 4, 5};
+        rev.setArray(originalArray);
+        rev.collectArrayIntoAMethodOrMutateItsState();
+        rev.reverseArray();
+    }
+
     public void setArray(int[] array){
         for(int index = 0; index<array.length; index++) {
             this.array[index] = array[index];
         }
+        Object
     }
     public int[] collectArrayIntoAMethodOrMutateItsState(){
         return array;
@@ -17,6 +26,7 @@ public class Rev {
         int arraySizeForIndex = arraySize - 1;
         int indexForNewArray = 0;
         for(int indexInArray = arraySizeForIndex; indexInArray >= 0; indexInArray--){
+            System.out.println(arraySizeForIndex + " " + arraySize);
             newReversedArray[indexForNewArray] = newArray[indexInArray];
         }
     }
