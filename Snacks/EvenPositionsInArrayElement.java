f4674e4670145dbffd77ab77e3c3d6598be3d6b9
Olawalesegun <olawalesegun0@gmail.com>
2023-05-18 23:52:11 +0100

    A program to check even positions in Array

--- Snacks/EvenPositionsInArrayElement.java
+++ Snacks/EvenPositionsInArrayElement.java
@@ -1,2 +1,27 @@
+import java.util.Arrays;
+
 public class EvenPositionsInArrayElement {
+
+    int[] arrayCollector = new int[7];
+    int[] arrayCollectingIndexAtEvenPositions = new int[arrayCollector.length];
+    public void setArrayToInitializeValueToInstanceVariable(int[] arrayCollector) {
+        this.arrayCollector = arrayCollector;
+    }
+
+    public int[] collectMutatedArray() {
+        return arrayCollector;
+    }
+
+    public void checkForElementsOnEVenPositionsInArray() {
+        int lengthOfArray = arrayCollector.length;
+        for(int indexOfPositionsWithinArray = 0; indexOfPositionsWithinArray<lengthOfArray; indexOfPositionsWithinArray++){
+            if(indexOfPositionsWithinArray % 2 == 0){
+                arrayCollectingIndexAtEvenPositions[indexOfPositionsWithinArray] = collectMutatedArray()[indexOfPositionsWithinArray];
+            }
+        }
+    }
+
+    public String returnElementsOnEVenPositionsInArray() {
+        return Arrays.toString(arrayCollectingIndexAtEvenPositions);
+    }
 }
