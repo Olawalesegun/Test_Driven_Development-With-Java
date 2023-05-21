6ca4802b6e79ae9dd55895c2042274e2a951be82
Olawalesegun <olawalesegun0@gmail.com>
2023-05-21 20:02:41 +0100

    Admin class for Estore

---
+++ OOP/oop_snack/Admin.java
@@ -0,0 +1,12 @@
+package oop_snack;
+
+public class Admin extends Users {
+    public Admin(String name, String emailAddress, int age, String password, String phoneNumber){
+        this.name = name;
+        this.emailAddress = emailAddress;
+        this.age = age;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+    }
+
+}
