b613c2787b5bb1e4c39d698ea8252ade7aa60d9c
Olawalesegun <olawalesegun0@gmail.com>
2023-05-21 20:05:02 +0100

    Credit Card Information for java

---
+++ OOP/oop_snack/CreditCardInformation.java
@@ -0,0 +1,22 @@
+package oop_snack;
+
+public class CreditCardInformation{
+
+    private String cvv;
+    private String cardExpirationYear;
+    private String cardExpirationMonth;
+    private String creditCardNumber;
+    private String nameOfCard;
+    private CardType cardType;
+
+    public CreditCardInformation(String cvv, String cardExpirationYear, String cardExpirationMonth,
+                                 String creditCardNumber, String nameOfCard, CardType cardType){
+        this.cvv = cvv;
+        this.cardExpirationYear = cardExpirationYear;
+        this.cardExpirationMonth = cardExpirationMonth;
+        this.creditCardNumber = creditCardNumber;
+        this.nameOfCard = nameOfCard;
+        this.cardType = cardType;
+
+    }
+}
