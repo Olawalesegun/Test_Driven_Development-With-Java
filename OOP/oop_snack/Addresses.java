23b34cf4815563598807c39044139929d0fa89e1
Olawalesegun <olawalesegun0@gmail.com>
2023-05-21 20:01:58 +0100

    Address class for Estore

---
+++ OOP/oop_snack/Addresses.java
@@ -0,0 +1,11 @@
+package oop_snack;
+
+public class Addresses {
+
+    private String cityName;
+    private String countryName;
+    private String houseNumber;
+    private String street;
+    private String state;
+
+}
