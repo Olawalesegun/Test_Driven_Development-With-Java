8facbd67a22d4d107414fe8c3884c2758fa55315
Olawalesegun <olawalesegun0@gmail.com>
2023-05-21 20:03:15 +0100

    Billing Information class for Estore

---
+++ OOP/oop_snack/BillingInformation.java
@@ -0,0 +1,16 @@
+package oop_snack;
+
+public class BillingInformation {
+    private String receiversPhoneNUmber;
+    private String receiversName;
+    private String deliveryAddress;
+    CreditCardInformation[] creditCardInformation;
+
+    public BillingInformation(String receiversName, String receiversPhoneNUmber, String deliveryAddress, CreditCardInformation[] creditCardInformation){
+        this.receiversName = receiversName;
+        this.receiversPhoneNUmber = receiversPhoneNUmber;
+        this.deliveryAddress = deliveryAddress;
+        this.creditCardInformation = creditCardInformation;
+    }
+
+}
