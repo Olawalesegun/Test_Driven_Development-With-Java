8a3d924b64ee5a743cfdb639a416fbf10a454ea8
Olawalesegun <olawalesegun0@gmail.com>
2023-05-21 20:05:54 +0100

    Customers Class for java

---
+++ OOP/oop_snack/Customers.java
@@ -0,0 +1,17 @@
+package oop_snack;
+
+import java.util.ArrayList;
+
+public class Customers extends Users{
+    private ArrayList<BillingInformation> billingInformation;
+    private ShoppingCart shoppingCart;
+    public Customers(String name, int age, String emailAddress, String password, String phoneNumber){
+       this.name = name;
+       this.age = age;
+       this.emailAddress = emailAddress;
+       this.password = password;
+       this.phoneNumber = phoneNumber;
+
+    }
+
+}
