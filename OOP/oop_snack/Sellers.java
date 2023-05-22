8f877f2698cb8aa37cdf6f95c8527e2b126e8077
Olawalesegun <olawalesegun0@gmail.com>
2023-05-22 02:04:39 +0100

    Shopping Cart for E-store App

---
+++ OOP/oop_snack/Sellers.java
@@ -0,0 +1,11 @@
+package oop_snack;
+
+public class Sellers extends Users{
+    public Sellers(String name,int age, String emailAddress, String password, String phoneNumber){
+        this.name = name;
+        this.age = age;
+        this.emailAddress = emailAddress;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+    }
+}
