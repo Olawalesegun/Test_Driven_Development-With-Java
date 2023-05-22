2a5c46213ef057d60119831b8900f4a14b564ede
Olawalesegun <olawalesegun0@gmail.com>
2023-05-22 02:03:09 +0100

    Product E-store OOP

---
+++ OOP/oop_snack/Product.java
@@ -0,0 +1,10 @@
+package oop_snack;
+
+public class Product {
+    private String productId;
+    private String productName;
+    private double price;
+    private String productDescription;
+    ProductCategory[] productCategory;
+
+}
