a5a09855eba77e3ac5037b02e3789acbcc0ae2c9
Olawalesegun <olawalesegun0@gmail.com>
2023-05-22 02:06:25 +0100

    Users cart for E-store App

---
+++ OOP/oop_snack/Users.java
@@ -0,0 +1,11 @@
+package oop_snack;
+
+public class Users {
+    protected int age;
+    protected String emailAddress;
+    protected String name;
+    protected String password;
+    protected String phoneNumber;
+
+
+}
