992400bda83d0ac9376d9f17d0b413e5117a090a
Olawalesegun <olawalesegun0@gmail.com>
2023-05-24 04:37:01 +0100

    credit card  validating -- first approach

--- OOP/creditCardValidator/CreditCardValidate.java
+++ OOP/creditCardValidator/CreditCardValidate.java
@@ -1,2 +1,31 @@
-package creditCardValidator;public class CreditCardValidate {
+package creditCardValidator;
+import java.util.Scanner;
+public class CreditCardValidate {
+
+    static Scanner userInput = new Scanner(System.in);
+    private static final CreditCardTemplate creditCard =  new CreditCardTemplate("Master-Card",
+            "4388576018402626", "Valid" );
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Kindly Enter Card details to verify");
+        String input = userInput.nextLine();
+        creditCard.setCardNumber(input);
+        String cred = creditCard.getCardNumber();
+        System.out.println("Credit Card Type: " + confirmCreditCardPatterns(cred));
+        System.out.println("Credit Card Number: " + cred);
+        System.out.println("Credit Card Digit Length: " +creditCard.getCardDigitLength());
+
+    }
+    public static String confirmCreditCardPatterns(String creditCardParameter){
+        int lengthOfParameter = creditCardParameter.length();
+        String typeOfCard = " ";
+       if(lengthOfParameter>13 && lengthOfParameter < 16){
+           if(creditCardParameter.startsWith("4")) typeOfCard = "Visa-Card";
+           else if (creditCardParameter.startsWith("5")) typeOfCard = "Master-Card";
+           else if(creditCardParameter.startsWith("37")) typeOfCard = "American-Express-Cards";
+           else if(creditCardParameter.startsWith("6")) typeOfCard = "Discover Cards";
+           else typeOfCard = "Kindly input the right Card";
+       }
+       return typeOfCard;
+    }
 }
