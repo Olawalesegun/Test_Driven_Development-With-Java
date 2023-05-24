047a02497299d2dc129e865779e0de94f04b6c19
Olawalesegun <olawalesegun0@gmail.com>
2023-05-24 17:09:03 +0100

    product source file

--- OOP/oop_snack/Product.java
+++ OOP/oop_snack/Product.java
@@ -6,5 +6,13 @@ public class Product {
     private double price;
     private String productDescription;
     ProductCategory[] productCategory;
+    Product(String productId, String productName,
+            double price, String productDescription, ProductCategory[] productCategory){
+        this.productId = productId;
+        this.productName = productName;
+        this.price = price;
+        this.productDescription = productDescription;
+        this.productCategory = productCategory;
+    }
 
 }
