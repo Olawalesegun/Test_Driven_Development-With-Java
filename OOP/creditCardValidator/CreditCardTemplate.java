60133bdba589b6f2a52820422d63c844ed2f6560
Olawalesegun <olawalesegun0@gmail.com>
2023-05-24 01:26:02 +0100

    a specific class for credit card details alone

--- OOP/creditCardValidator/CreditCardTemplate.java
+++ OOP/creditCardValidator/CreditCardTemplate.java
@@ -1,30 +1,32 @@
 package creditCardValidator;
 
-public class CreditCardValidateTemplate {
+public class CreditCardTemplate {
 
     private String cardType;
     private String cardNumber;
     private int cardDigitLength;
     private String cardValidityStatus;
 
-    public CreditCardValidateTemplate(String cardType, String cardNumber,
-                                      int cardDigitLength, String cardValidityStatus){
+    public CreditCardTemplate(String cardType, String cardNumber, String cardValidityStatus){
         this.cardType = cardType;
         this.cardNumber = cardNumber;
-        this.cardDigitLength = cardDigitLength;
+        this.cardDigitLength = cardNumber.length();
         this.cardValidityStatus = cardValidityStatus;
     }
     public void setCardType(String cardType){
         this.cardType = cardType;
     }
-    public String getCardTyp(){
+    public String getCardType(){
         return cardType;
     }
     public void setCardNumber(String cardNumber){
         this.cardNumber = cardNumber;
     }
-    public String cardNumber(){
-        return cardNumber
+    public String getCardNumber(){
+        return cardNumber;
+    }
+    public int getCardDigitLength(){
+        return getCardNumber().length();
     }
 
 }
