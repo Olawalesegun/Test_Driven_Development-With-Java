c1ea54f7836248848553afe85887d36379d4ae28
Olawalesegun <olawalesegun0@gmail.com>
2023-05-26 03:39:51 +0100

    Question raised for MBTI personality trait test

--- OOP/myersbriggs/Question.java
+++ OOP/myersbriggs/Question.java
@@ -1,2 +1,18 @@
-package myersbriggs;public class Question {
+package myersbriggs;
+
+public class Question {
+    private String question;
+
+    public Question(String question){
+        this.question = question;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("%s",getQuestion());
+    }
 }
