67123261acea3e5d9d4108cfc739c1a6dd48ada8
Olawalesegun <olawalesegun0@gmail.com>
2023-05-26 02:28:53 +0100

    OOP and getting familiar with it

---
+++ OOP/School.java
@@ -0,0 +1,13 @@
+public enum School {
+
+    LASU("Lagos state university secondary school"),
+    LASPOTECH("Lagos state poly primary school"),
+    UNN("University of Lagos is in Akoka");
+
+    private String fullName;
+
+    School(String fullName){
+        this.fullName = fullName;
+    }
+
+}
