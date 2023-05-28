2123686c136906666136a1385ce3598a169976df
Olawalesegun <olawalesegun0@gmail.com>
2023-05-28 23:17:37 +0100

    Nigeria Geo Political Zones Test

---
+++ test/NigeriaGeoPoliticalZonesTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NigeriaGeoPoliticalZonesTest {
+
+    @Test
+    public void testForSouthSouth(){
+        NigeriaGeoPoliticalZones nig = NigeriaGeoPoliticalZones.SOUTH_SOUTH;
+
+    }
+}
