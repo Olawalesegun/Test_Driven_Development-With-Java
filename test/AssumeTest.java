7e4cfacf0f297aacbf7e06cac93d7aab6960aa26
Olawalesegun <olawalesegun0@gmail.com>
2023-05-28 17:07:08 +0100

    a program that takes in 2 parameters and multiplies it without using the multiplication operator or the multiply inbuilt method

--- test/AssumeTest.java
+++ test/AssumeTest.java
@@ -3,7 +3,6 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AssumeTest {
-
     @Test
     public void Result1Test(){
         Assume assume = new Assume();
@@ -11,14 +10,25 @@ class AssumeTest {
         int multiplicationResult = assume.getMultiplicationOfParametersEntered();
         assertEquals(12, multiplicationResult);
     }
-
     @Test
-    public void Result2Test(){
+    public void testResultForTheRightOperandBeingNegative(){
         Assume assume = new Assume();
-        assume.setParametersToMultiply(6, -4);
+        assume.setParametersToMultiply(6, -2);
+        int multiplicationResult = assume.getMultiplicationOfParametersEntered();
+        assertEquals(-12, multiplicationResult);
+    }
+    @Test
+    public void testResultForTheLeftOperandBeingNegative(){
+        Assume assume = new Assume();
+        assume.setParametersToMultiply(-6, 2);
+        int multiplicationResult = assume.getMultiplicationOfParametersEntered();
+        assertEquals(-12, multiplicationResult);
+    }
+    @Test
+    public void testResultForBothOperandBeingNegative2Test(){
+        Assume assume = new Assume();
+        assume.setParametersToMultiply(-6, -4);
         int multiplicationResult = assume.getMultiplicationOfParametersEntered();
         assertEquals(24, multiplicationResult);
     }
-
-
 }
