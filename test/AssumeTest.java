accd2cce86d4d01ea51941ad05d4c4030e8f40ba
Olawalesegun <olawalesegun0@gmail.com>
2023-05-28 16:28:42 +0100

    a program that takes in 2 parameters and multiplies it without using the multiplication operator or the multiply inbuilt method

--- test/AssumeTest.java
+++ test/AssumeTest.java
@@ -1,5 +1,24 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AssumeTest {
+
+    @Test
+    public void Result1Test(){
+        Assume assume = new Assume();
+        assume.setParametersToMultiply(6, 2);
+        int multiplicationResult = assume.getMultiplicationOfParametersEntered();
+        assertEquals(12, multiplicationResult);
+    }
+
+    @Test
+    public void Result2Test(){
+        Assume assume = new Assume();
+        assume.setParametersToMultiply(6, -4);
+        int multiplicationResult = assume.getMultiplicationOfParametersEntered();
+        assertEquals(24, multiplicationResult);
+    }
+
 
 }
