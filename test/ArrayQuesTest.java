d66cac426e34955de35aa0eb971cc47103c2e460
Olawalesegun <olawalesegun0@gmail.com>
2023-05-28 16:01:14 +0100

    In this file, An approach to TDD was carried out where I tested for different scenarios for the array task

--- test/ArrayQuesTest.java
+++ test/ArrayQuesTest.java
@@ -1,5 +1,43 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ArrayQuesTest {
+
+    @Test
+    public void testArrayToReturnDouble(){
+        ArrayQues arrQue = new ArrayQues();
+        int[] arrPassed = {1, 2, 3};
+        arrQue.takeInArray(arrPassed);
+        int[] returnArrayDoubled = arrQue.doubledArrayReturned();
+        int[] expectedResult = {1, 2, 3, 0, 0, 0};
+        assertArrayEquals(expectedResult, returnArrayDoubled);
+
+    }
+    @Test
+    public void testArrayToReturnDouble2(){
+        ArrayQues arrQue = new ArrayQues();
+        int[] arrPassed = {1, 2, 3, 4};
+        arrQue.takeInArray(arrPassed);
+        int[] returnArrayDoubled = arrQue.doubledArrayReturned();
+        int[] expectedResult = {1, 2, 3, 4, 0, 0, 0, 0};
+        assertArrayEquals(expectedResult, returnArrayDoubled);
+
+    }
+    @Test
+    public void testArrayToReturnDouble3(){
+        ArrayQues arrQue = new ArrayQues();
+        int[] arrPassed = {1, 2,};
+        arrQue.takeInArray(arrPassed);
+        int[] returnArrayDoubled = arrQue.doubledArrayReturned();
+        int[] expectedResult = {1, 2,0, 0};
+        assertArrayEquals(expectedResult, returnArrayDoubled);
+
+    }
+//    @Test
+//    public void testArrayToReturnDouble4(){
+//        ArrayQues arrQue = new ArrayQues();
+//        int
+//    }
 
 }
