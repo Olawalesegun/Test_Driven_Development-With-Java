9b844865d1b0d11b223d482f1782b4585cd1cde9
Olawalesegun <olawalesegun0@gmail.com>
2023-05-29 15:15:12 +0100

    Java interface

--- OOP/InterfaceWithList/JavaList.java
+++ OOP/InterfaceWithList/JavaList.java
@@ -1,4 +1,13 @@
 package InterfaceWithList;
 
-public interface List {
+import java.util.Collection;
+
+public interface JavaList{
+
+    void add(String item);
+    void add(int index, String element);
+    String get(int index);
+    void remove(String element);
+    void remove(int index);
+    int size();
 }
