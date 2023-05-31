54f11fe3ca7e7049c67e827a0504dcc9210f5cac
Olawalesegun <olawalesegun0@gmail.com>
2023-05-31 22:52:44 +0100

    implementing Stack as a Data Structure

--- DSA/DataStructuresAndAlgorithm/MyStack.java
+++ DSA/DataStructuresAndAlgorithm/MyStack.java
@@ -1,4 +1,25 @@
-package dataStructuresAndAlgorithm;
+package DataStructuresAndAlgorithm;
 
 public class MyStack {
+    private boolean isEmpty = true;
+    private int counter;
+    private String[] elements;
+    public MyStack(int sizeOfStack) {
+        elements = new String[sizeOfStack];
+    }
+
+    public boolean isEmpty() {
+        if(counter == 0) return true;
+        return false;
+    }
+
+    public void push(String element) {
+        elements[counter] = element;
+        counter++;
+    }
+
+    public String pop() {
+       counter--;
+       return elements[counter];
+    }
 }
