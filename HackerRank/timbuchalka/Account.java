48410f40bcecfb7a5ee8fd0673df096584bbb53d
Olawalesegun <olawalesegun0@gmail.com>
2023-05-31 23:31:25 +0100

    a program that implements an Account

--- HackerRank/timbuchalka/Account.java
+++ HackerRank/timbuchalka/Account.java
@@ -1,2 +1,32 @@
-package timbuchalka;public class Account {
+package timbuchalka;
+
+public class Account {
+
+    private String phoneNumber;
+    private String email;
+    private String customerName;
+    private double accountBalance;
+    private String accountNumber;
+
+    public Account(String accountNumber, double accountBalance, String customerName, String email, String phoneNumber) {
+        this.accountNumber = accountNumber;
+        this.accountBalance = accountBalance;
+        this.customerName = customerName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double checkAccountBalance(String accountNumber) {
+        double balance = 0;
+        if (accountNumber.equals(this.accountNumber)) balance = accountBalance;
+        return balance;
+    }
 }
