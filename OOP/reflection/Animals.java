c69d89adde914a75f555881f1d558c8fe5f4f767
Olawalesegun <olawalesegun0@gmail.com>
2023-05-31 23:47:59 +0100

    Animals oop redefined

--- OOP/reflection/Animals.java
+++ OOP/reflection/Animals.java
@@ -1,2 +1,11 @@
-package reflection;public class Animals {
+package reflection;
+
+public class Animals extends LivingThings{
+    private Vertebrate vertebrate;
+    private Invertebrates invertebrate;
+
+    public Animals(){
+        System.out.println("I am trying to create a Living thing");
+    }
+
 }
