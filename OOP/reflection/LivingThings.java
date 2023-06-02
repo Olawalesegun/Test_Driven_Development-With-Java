3b9c9c36993e2e2a0294eb05b558dcfc5b4b996a
Olawalesegun <olawalesegun0@gmail.com>
2023-06-02 21:10:06 +0100

    Living things ---

--- OOP/reflection/LivingThings.java
+++ OOP/reflection/LivingThings.java
@@ -1,2 +1,11 @@
-package reflection;public class LivingThings {
+package reflection;
+
+public class LivingThings extends Things{
+    private Plants plant;
+    private Animals animal;
+
+    public void move(){
+        System.out.println("Swimming");
+    }
+
 }
