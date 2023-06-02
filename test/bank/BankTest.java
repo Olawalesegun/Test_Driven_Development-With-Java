f5846c211679190f62f0e1734309351c86e7130c
Olawalesegun <olawalesegun0@gmail.com>
2023-06-02 04:36:58 +0100

    a test case to test the Banks functionality

--- test/bank/BankTest.java
+++ test/bank/BankTest.java
@@ -1,6 +1,40 @@
 package bank;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class BankTest {
-  
+
+    Bank sterlingBank;
+
+    @BeforeEach
+    void setUp() {
+        sterlingBank = new Bank();
+    }
+
+    @Test
+    public void testThatAccountCanBeCreated(){
+        assertNotNull(sterlingBank);
+    }
+    @Test
+    public void testThatBankCanRegisterNewCustomer() {
+        sterlingBank.registerNewCustomer("Tunde", "orangekapital@include.com");
+        //String email = sterlingBank.getBankAccName();
+        assertEquals("orangekapital@include.com", sterlingBank.getBankAccEmail());
+    }
+    @Test
+    public void testThatBankCanRegisterNewCustomer2(){
+        sterlingBank.registerNewCustomer("Funmi", "fedalshoot@inc.com");
+        String name = sterlingBank.getBankAccName();
+        assertEquals("Funmi", name);
+    }
+    @Test
+    public void testToConfirmDepositFunctionality(){
+        sterlingBank.registerNewCustomer("Shola", "sholatech@gig.inc");
+        String accNo = sterlingBank.getAccNumber();
+        sterlingBank.deposit(accNo, 3000);
+        assertEquals(3000, sterling.getBalance());
+    }
 }
