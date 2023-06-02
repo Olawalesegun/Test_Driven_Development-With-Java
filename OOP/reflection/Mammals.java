42a2260e72e154866e06d5783074a3d9ab3412cb
Olawalesegun <olawalesegun0@gmail.com>
2023-06-02 21:10:46 +0100

    Mammals ----

--- OOP/reflection/Mammals.java
+++ OOP/reflection/Mammals.java
@@ -1,2 +1,16 @@
-package reflection;public class Mammals {
+package reflection;
+
+public class Mammals extends Vertebrate{
+
+    public Mammals(){
+//        super()
+        System.out.println("I am trying to create a mamal");
+    }
+    public void move(){
+        System.out.println("Cat walk wan kill victoria");
+    }
+    public void sleep(){
+        System.out.println("Baba Segun wan sleep again");
+    }
 }
+
