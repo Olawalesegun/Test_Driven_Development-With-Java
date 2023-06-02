f6d32d7f42b850566dc6d063f86a56365e27cc78
Olawalesegun <olawalesegun0@gmail.com>
2023-06-02 21:14:30 +0100

    Vertebrate ------

--- OOP/reflection/Vertebrate.java
+++ OOP/reflection/Vertebrate.java
@@ -1,2 +1,20 @@
-package reflection;public class Vertebrate {
+package reflection;
+
+import java.util.List;
+
+public class Vertebrate extends Animals{
+    private Mammals mammals;
+    private Aves aves;
+    private Reptiles reptiles;
+    private Amphibians amphibians;
+    private Pisces pisces;
+    /*private List<Mammals> mammals;
+    private List<Aves> aves;
+    private List<Reptiles> reptile;
+    private List<Amphibians> amphibians;
+    private List<Pisces> pisces;*/
+
+    public Vertebrate(){
+        System.out.println("I am trying to create a Vertebrate");
+    }
 }
