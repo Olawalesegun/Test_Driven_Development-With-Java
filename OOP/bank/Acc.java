aac5915bd1ae97bf48b7bf37b828d978f7ea937c
Olawalesegun <olawalesegun0@gmail.com>
2023-06-03 23:15:42 +0100

    Account class ----> To Be reviewed soon

--- OOP/bank/Acc.java
+++ OOP/bank/Acc.java
@@ -1,2 +1,11 @@
-package bank;public interface Acc {
+package bank;
+
+public class Acc {
+
+    void deposit(int amount){
+    }
+    int withdraw(String accountNumber, int amountToBeWithdrawn){
+    }
+    String checkBalance(String accountNumber){
+    }
 }
