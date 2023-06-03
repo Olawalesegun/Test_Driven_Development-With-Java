4f2c5d08b25cb7b2811c3e1b714b0e2ea4087164
Olawalesegun <olawalesegun0@gmail.com>
2023-06-03 23:19:09 +0100

    Still working on DIary App

--- OOP/diaryApp/Diary.java
+++ OOP/diaryApp/Diary.java
@@ -1,4 +1,32 @@
 package diaryApp;
 
 public class Diary {
+    private boolean isLocked = true;
+    private String password;
+    private int gistsCount;
+    public Diary(String username, String password) {
+        this.password = password;
+    }
+
+    public boolean isLocked() {
+        return isLocked;
+    }
+
+    public void unlockWith(String password) {
+        boolean passwordIsCorrect = password.equals(this.password);
+        if(passwordIsCorrect) isLocked = false;
+    }
+    public void unlockDiary(){
+        isLocked = false;
+    }
+
+    public void lock() {
+        isLocked = true;
+    }
+    public void addGist(String title, String body){
+        gistsCount++;
+    }
+    public int countGists(){
+        return gistsCount;
+    }
 }
