25a2644e1a2a0bf27220106474e561c5bdba6d35
Olawalesegun <olawalesegun0@gmail.com>
2023-06-05 01:46:15 +0100

    a program to simulate te bank

--- OOP/bank/Bank.java
+++ OOP/bank/Bank.java
@@ -13,14 +13,66 @@ public class Bank {
     public Account registerNewCustomer(String firstName, String lastName, String email, String password){
         Account newAccCreated = null;
         if(validateRegistrationDetails(firstName, lastName, email, password)){
-            String accountNumber = 10000000 + "" + countAndIncrementUponCreationOfAccount;
+            String accountNumber = 100000000 + countAndIncrementUponCreationOfAccount + "";
             newAccCreated = new Account(accountNumber,firstName, lastName, email, password);
             listOfAccountsPresentInBank.add(countAndIncrementUponCreationOfAccount, newAccCreated);
             countAndIncrementUponCreationOfAccount++;
         }
         return newAccCreated;
     }
-    public void deposit
+    public void deposit(String accNo, double amount){
+        if(verifyAccountNumber(accNo) && verifyAmountIsValid(amount)) {
+            for (Account acc : listOfAccountsPresentInBank) {
+                if (accNo.equals(acc.getAccountNumber())) {
+                    acc.deposit(amount);
+                }
+            }
+        }
+    }
+    public void withdraw(String accNo, double amount, String password){
+        if(verifyAccountNumber(accNo) && verifyAmountIsValid(amount) &&verifyPassword(password)){
+            for (Account acc : listOfAccountsPresentInBank){
+                    if (accNo.equals(acc.getAccountNumber()) && password.equals(acc.getAccountPassword())) {
+                        acc.withdraw(password, amount);
+                        break;
+                    }
+            }
+        }
+    }
+    public void transfer(String sendersAccountNo, String receiversAccountNumber, double amount, String password){
+        if(verifyAccountNumber(sendersAccountNo) && verifyAccountNumber(receiversAccountNumber) &&
+                verifyAmountIsValid(amount) && verifyPassword(password)){
+            Account senderAccountRecord = lookForAccountByTheAccountNumber(sendersAccountNo);
+            Account receiverAccountRecord = lookForAccountByTheAccountNumber(receiversAccountNumber);
+            if(senderAccountRecord != null && receiverAccountRecord != null &&
+                    senderAccountRecord.getAccountPassword().equals(password)){
+                senderAccountRecord.withdraw(password, amount);
+                receiverAccountRecord.deposit(amount);
+            }
+        }
+    }
+
+    private Account lookForAccountByTheAccountNumber(String receiversAccountNumber) {
+        for(Account acc: listOfAccountsPresentInBank){
+            if(acc.getAccountNumber().equals(receiversAccountNumber)){
+                return acc;
+            }
+        }
+        return null;
+    }
+
+    public boolean verifyPassword(String password){
+        boolean isPasswordVerified = password.length() == 5;
+        return isPasswordVerified;
+    }
+    public boolean verifyAmountIsValid(double amount) {
+        boolean isAmountValid = amount > 0;
+        return isAmountValid;
+    }
+    private boolean verifyAccountNumber(String accNo){
+        boolean isAccLengthTheRequiredLength = accNo.length() == 10;
+        return isAccLengthTheRequiredLength;
+    }
     public boolean validateRegistrationDetails(String firstName, String lastName, String email, String password){
         boolean verifyFirstName = firstName.length() > 2;
         boolean verifyLastName = lastName.length() > 2;
