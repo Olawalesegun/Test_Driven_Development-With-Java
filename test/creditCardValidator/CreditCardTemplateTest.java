463dcd1e3b5bf83361323442577566ff4b9c4250
Olawalesegun <olawalesegun0@gmail.com>
2023-06-10 20:15:41 +0100

    Cred card

--- test/creditCardValidator/CreditCardTemplateTest.java
+++ test/creditCardValidator/CreditCardTemplateTest.java
@@ -1,6 +1,40 @@
 package creditCardValidator;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CreditCardTemplateTest {
-  
+    CreditCardTemplate cct;
+
+    @BeforeEach
+    void setUp() {
+        cct = new CreditCardTemplate("VISA", "419867534218", "Valid");
+    }
+
+    @Test
+    public void testConstructorPresence(){
+        assertNotNull(cct);
+    }
+    @Test
+    public void testCardType(){
+        cct.setCardType("Master-Card");
+       String cardType = cct.getCardType();
+       assertEquals("Master-Card", cardType);
+    }
+    @Test
+    public void testCardNumber(){
+        cct.setCardNumber("4198897978575");
+        cct.getCardNumber();
+        assertEquals("4198897978575",  cct.getCardNumber());
+    }
+    @Test
+    public void testCardLength(){
+        cct.setCardNumber("4198897978575");
+        cct.getCardNumber();
+        cct.getCardDigitLength();
+        assertEquals(13, cct.getCardDigitLength());
+    }
+
 }
