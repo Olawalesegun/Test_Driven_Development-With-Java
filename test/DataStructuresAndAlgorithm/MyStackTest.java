76bc661319381a5de893fff90999743aa18d5206
Olawalesegun <olawalesegun0@gmail.com>
2023-06-10 20:16:34 +0100

    MyStacl List

--- test/DataStructuresAndAlgorithm/MyStackTest.java
+++ test/DataStructuresAndAlgorithm/MyStackTest.java
@@ -1,7 +1,54 @@
-package dataStructuresAndAlgorithm;
+package DataStructuresAndAlgorithm;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MyStackTest {
+    MyStack  stack;
+    @BeforeEach
+    void setUp() {
+        stack = new MyStack(5);
+    }
+    @Test
+    public void stackExistTest(){
+        assertNotNull(stack);
+    }
+    @Test
+    public void pushXPopXStackShouldBeEmptyTest(){
+        MyStack stack = new MyStack(5);
+        stack.push("E-String");
+        stack.pop();
+        assertTrue(stack.isEmpty());
+    }
+    @Test public void pushXAndYPopYStackShouldNotBeEmptyTest(){
+        MyStack stack = new MyStack(5);
+        stack.push("G-String");
+        stack.push("E-String");
+        stack.pop();
+        assertFalse(stack.isEmpty());
+    }
+    @Test public void pushX_shouldeturnX__whenPopped_test(){
+        stack.push("D-String");
+        stack.push("E-String");
+        stack.push("G-String");
+        String actual = stack.pop();
+        assertEquals("G-String", actual);
+    }
+    @Test public void pushXYZ_shouldReturnY__whenPoppedTwice_test(){
+        stack.push("D-String");
+        stack.push("E-String");
+        stack.push("G-String");
+        String actual = stack.pop();
+        assertEquals("G-String", actual);
+    }
+
+   /* @Test
+    public void newStackIsEmptyTest(){
+        *//*  MyStack stack = new MyStack(5);*//*
+        assertTrue(stack.isEmpty());
+    }*/
+
 
 }
