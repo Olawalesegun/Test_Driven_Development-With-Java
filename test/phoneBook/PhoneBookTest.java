502b74e7bac84970813585189aa972c498fb451c
Olawalesegun <olawalesegun0@gmail.com>
2023-06-13 21:49:31 +0100

    a test case to test phoneBook and ensure it meets requirement

--- test/phoneBook/PhoneBookTest.java
+++ test/phoneBook/PhoneBookTest.java
@@ -3,6 +3,8 @@ package phoneBook;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PhoneBookTest {
@@ -26,12 +28,15 @@ class PhoneBookTest {
     public void testThatICanCanCreateMoreThanOneContact(){
         tecnoPhoneBook.createContact("Lamide", "Kelvin", "09053010203",
                 "lam@ir.com", "13-02-2009");
-        PhoneBook samsungPhoneBook = new PhoneBook();
-        samsungPhoneBook.createContact("Anjola", "Salsa", "07065432108",
-                "anj@kel.com", "12-01-1990");
-        assertEquals("Lamide", tecnoPhoneBook.getFirstName());
-        assertEquals("Anjola", samsungPhoneBook.getFirstName());
+        tecnoPhoneBook.createContact("Anjola", "Salsa", "07065432108",
+               "anj@kel.com", "12-01-1990");
+     String newElements = tecnoPhoneBook.displayContacts();
+         String[] arr = {"Lamide Kelvin 09053010203 09053010203 lam@ir.com, Anjola Salsa 07065432108 07065432108 anj@kel.com"};
+     assertEquals( Arrays.toString(arr) , newElements);
+
     }
+
+
    /* @Test
     public void testThatICanAddContact(){
         tecnoPhoneBook.createContact();
