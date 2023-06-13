9adaf1c986a486e15e2556165022c19f278e03c5
Olawalesegun <olawalesegun0@gmail.com>
2023-06-13 19:39:28 +0100

    Contacts ----

--- OOP/phoneBook/Contacts.java
+++ OOP/phoneBook/Contacts.java
@@ -9,7 +9,7 @@ public class Contacts {
 
     public Contacts(){
     }
-    public void createContact(String firstName, String lastName, String phoneNo, String email, String bDay){
+    public Contacts(String firstName, String lastName, String phoneNo, String email, String bDay){
         this.firstName = firstName;
         this.lastName = lastName;
         this.phoneNo = phoneNo;
@@ -19,23 +19,31 @@ public class Contacts {
     public void updateFirstName(String firstName){
         this.firstName = firstName;
     }
-
+    public void updateLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public void updateEmail(String mail) {
+        this.email = email;
+    }
+    public void updatePhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
     public String getFirstName(){
         return firstName;
     }
-
     public String getLastName() {
         return lastName;
     }
     public String getPhoneNo() {
         return phoneNo;
     }
-
     public String getDate() {
         return bDay;
     }
 
-    public void updateLastName(String lastName) {
-        this.lastName = lastName;
+
+
+    public String getEmail() {
+        return email;
     }
 }
