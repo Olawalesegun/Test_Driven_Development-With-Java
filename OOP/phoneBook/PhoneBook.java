e409d86444955f10d4cc141fb01ed9c7f177c0d1
Olawalesegun <olawalesegun0@gmail.com>
2023-06-15 00:12:53 +0100

    Here is a program that simulates the phoneBook App

--- OOP/phoneBook/PhoneBook.java
+++ OOP/phoneBook/PhoneBook.java
@@ -1,33 +1,63 @@
 package phoneBook;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class PhoneBook {
 
     private ArrayList<Contacts> contactList;
     private Contacts newContact;
-
     public PhoneBook() {
         contactList = new ArrayList<>();
     }
-
     public void createContact(String firstName, String lastName, String phoneNo, String mail, String bDay) {
         newContact = new Contacts(firstName, lastName, phoneNo, mail, bDay);
         contactList.add(newContact);
     }
-
     public void removeContact(String firstName) {
         for (Contacts cont : contactList) {
-            if (firstName.equals(cont)) {
-                contactList.remove(firstName);
+            if (cont.getFirstName().equals(firstName)) {
+                contactList.remove(cont);
             } else {
                 System.out.println(" ---- Name cannot be found -------");
             }
         }
     }
-
     public String getFirstName() {
         return newContact.getFirstName();
+    }
+    public String displayContacts() {
+        String[] elementsInContact = new String[contactList.size()];
+        int count = 0;
+        for(Contacts cont: contactList) {
+            elementsInContact[count] = printElementsOfPhoneBook(cont);
+            count++;
+            //System.out.println(cont);
+        }
+        return Arrays.toString(elementsInContact);
+    }
+    public String  printElementsOfPhoneBook(Contacts con){
+        String elementsContainer = con.getFirstName() + " " +con.getLastName() + " " +
+                con.getPhoneNo() + " " + con.getEmail() + " " + con.getDate();
+        //print(elementsContainer);
+        return elementsContainer;
+    }
+    public void print(String elementToPrint){
+        System.out.println(elementToPrint);
+    }
+
+    public int getSize() {
+        return contactList.size();
+    }
+
+    public String searchContactFor(String nameInContact) {
+        String result = null;
+        for(Contacts contact: contactList){
+            if(contact.getFirstName().equals(nameInContact)){
+                result = printElementsOfPhoneBook(contact);
+            }
+        }
+        return result;
     }
 }
 
