69c99c861c4193b3d2568f36e362389d4749b081
Olawalesegun <olawalesegun0@gmail.com>
2023-07-20 18:17:11 +0100

    Main --

--- teach/numerals/Main.java
+++ teach/numerals/Main.java
@@ -1,2 +1,19 @@
-package numerals;public class Main {
+package numerals;
+
+public class Main {
+    public static void main(String[] args) {
+        Octal numeral = new Octal();
+        System.out.print("The octal value for 014 is:");
+        Octal.print(numeral.getNumeralNumber1());
+        System.out.print("The octal value for 067 is: ");
+        Octal.print(numeral.getNumeralNumber2());
+        System.out.print("The octal value for 01000 is: ");
+        Octal.print(numeral.getNumeralNumber3());
+        System.out.print("The octal value for 0677 is: ");
+        Octal.print(numeral.getNumeralNumber4());
+        System.out.print("The octal value for 0777 is: ");
+        Octal.print(numeral.getNumeralNumber5());
+        System.out.print("The octal value for 0655 is: ");
+        Octal.print(numeral.getNumeralNumber6());
+    }
 }
