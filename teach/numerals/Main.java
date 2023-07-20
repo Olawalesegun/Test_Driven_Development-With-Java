bd642e290d9a3403b7ae5333c47d8b389c4830ae
Olawalesegun <olawalesegun0@gmail.com>
2023-07-20 18:16:43 +0100

    teaching Octals and also practicing Octal numeral system

---
+++ teach/numerals/Main.java
@@ -0,0 +1,2 @@
+package numerals;public class Main {
+}
---
+++ teach/numerals/Octal.java
@@ -0,0 +1,40 @@
+package numerals;
+
+public class Octal {
+
+    final private static int numeralNumber1 = 014;
+    final private static int numeralNumber2 = 067;
+    final private static int numeralNumber3 = 01000;
+    final private static int numeralNumber4 = 0677;
+    final private static int numeralNumber5 = 0777;
+    final private static int numeralNumber6 = 0655;
+
+
+    public static void print(int num){
+        System.out.println(num);
+    }
+    public int getNumeralNumber1(){
+        return numeralNumber1;
+    }
+    public int getNumeralNumber2(){
+        return numeralNumber2;
+    }
+    public int getNumeralNumber3(){
+        return numeralNumber2;
+    }
+    public int getNumeralNumber4(){
+        return numeralNumber2;
+    }
+    public int getNumeralNumber5(){
+        return numeralNumber2;
+    }
+    public int getNumeralNumber6(){
+        return numeralNumber2;
+    }
+
+
+    /*public static void main(String[] args) {
+        Numerals num = new Numerals();
+        print(num.getNumeralNumber());
+    }*/
+}
