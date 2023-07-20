02a1592acbf52a21c1aa4629dc78ddd4b96f6bbb
Olawalesegun <olawalesegun0@gmail.com>
2023-07-20 19:54:40 +0100

    Main --

---
+++ teach/numerals/Base2.java
@@ -0,0 +1,2 @@
+package numerals;public class Base2 {
+}
--- teach/numerals/Main.java
+++ teach/numerals/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Octal numeral = new Octal();
         HexaDecimal hex = new HexaDecimal();
+        Base2 base2 = new Base2();
 
         System.out.print("The octal value for 014 is:");
         Octal.print(numeral.getNumeralNumber1());
@@ -18,13 +19,20 @@ public class Main {
         System.out.print("The octal value for 0655 is: ");
         Octal.print(numeral.getNumeralNumber6());
 
-        System.out.println("==============================");
+        System.out.println("===================================");
 
-        /*System.out.println("The hexadecimal value for 0x64");
-        HexaDecimal.print(hex.getHexNum1());*/
+        System.out.println("The hexadecimal value for 0x64");
+        HexaDecimal.print(hex.getHexNum1());
         System.out.println("The hexadecimal value for 0x99");
-        HexaDecimal.print(hex.getHexNum1());
+        HexaDecimal.print(hex.getHexNum2());
         System.out.println("The hexadecimal value for 0x899");
-        HexaDecimal.print(hex.getHexNum1());
+        HexaDecimal.print(hex.getHexNum3());
+
+        System.out.println("=====================================");
+
+        System.out.println("The base 2 value for 0b1111");
+        Base2.print(base2.getNumber1());
+        System.out.println("The base 2 value for 0b1000");
+        Base2.print(base2.getNumber2());
     }
 }
