c2c7ecee64c7375a12d7b3e8c8254b17c69796f3
Olawalesegun <olawalesegun0@gmail.com>
2023-07-20 18:52:00 +0100

    teaching hexa decimal and also practicing and understanding under the hood for  numeral system

--- teach/numerals/HexaDecimal.java
+++ teach/numerals/HexaDecimal.java
@@ -1,17 +1,17 @@
 package numerals;
 
 public class HexaDecimal {
-    private final static int hexNum1 = 0x64;
-    private final static int hexNum2 = 0x99;
-    private final static int hexNum3 = 0x899;
+    private int hexNum1 = 0x64;
+    private int hexNum2 = 0x99;
+    private int hexNum3 = 0x899;
 
-    public static int getHexNum1() {
+    public int getHexNum1() {
         return hexNum1;
     }
-    public static int getHexNum2() {
+    public int getHexNum2() {
         return hexNum2;
     }
-    public static int getHexNum3() {
+    public int getHexNum3() {
         return hexNum3;
     }
     public static void print(int num){
