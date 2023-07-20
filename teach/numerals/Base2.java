c5e576ab60d80fdf2e547aea71b4bf95feeeeb1f
Olawalesegun <olawalesegun0@gmail.com>
2023-07-20 19:55:36 +0100

    Base 2 numeral system

--- teach/numerals/Base2.java
+++ teach/numerals/Base2.java
@@ -1,2 +1,17 @@
-package numerals;public class Base2 {
+package numerals;
+
+public class Base2 {
+    private int number1 = 0b1111;
+    private int number2 = 0b1000;
+
+
+    public int getNumber1() {
+        return number1;
+    }
+    public int getNumber2(){
+        return number2;
+    }
+    public static void print(int num){
+        System.out.println(num);
+    }
 }
