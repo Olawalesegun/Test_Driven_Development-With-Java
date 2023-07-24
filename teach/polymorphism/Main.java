cdea08282be5ee968908bb82a5b1afab71046cd4
Olawalesegun <olawalesegun0@gmail.com>
2023-07-24 20:37:46 +0100

    miain

--- teach/polymorphism/Main.java
+++ teach/polymorphism/Main.java
@@ -1,2 +1,15 @@
-package polymorphism;public class Main {
+package polymorphism;
+
+public class Main {
+    public static void main(String[] args) {
+        Elephant elephant = new Elephant();
+        Frog2 frog = new Frog2();
+        Goat2 goat = new Goat2();
+
+        Animal2 animal = new Animal2();
+        PlanetEarth planetEarth = new PlanetEarth();
+        planetEarth.whatCanAnimalDo(elephant);
+        planetEarth.whatCanAnimalDo(frog);
+        planetEarth.whatCanAnimalDo(goat);
+    }
 }
