1ff6c60f60b30f36f1a16fcf4206de9fc417d67a
Olawalesegun <olawalesegun0@gmail.com>
2023-07-24 20:37:23 +0100

    plomyphism with human as a class

--- teach/polymorphism/Goat2.java
+++ teach/polymorphism/Goat2.java
@@ -1,2 +1,10 @@
-package polymorphism;public class Goat2 {
+package polymorphism;
+
+public class Goat2 extends Animal2{
+    public void drinkWater(){
+        System.out.println("As a Goat, I drink water");
+    }
+    public void makeSound(){
+        System.out.println("As a Goat, I make bleet");
+    }
 }
