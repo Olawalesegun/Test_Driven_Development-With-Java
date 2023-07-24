2a8ad993c045b900cc8ad971a1858d11e30e63b8
Olawalesegun <olawalesegun0@gmail.com>
2023-07-24 20:35:56 +0100

    Frog -- polymorphism

--- teach/polymorphism/Frog2.java
+++ teach/polymorphism/Frog2.java
@@ -1,2 +1,10 @@
-package polymorphism;public class Frog2 {
+package polymorphism;
+
+public class Frog2 extends Animal2{
+    public void drinkWater(){
+        System.out.println("As a Frog, I drink water");
+    }
+    public void makeSound(){
+        System.out.println("As a Frog, I make croc sound");
+    }
 }
