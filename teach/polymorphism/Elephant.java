31de2e20df8ce2862fc2bd840e8520ef4ce18364
Olawalesegun <olawalesegun0@gmail.com>
2023-07-24 20:35:12 +0100

    ele -- polymorphism

--- teach/polymorphism/Elephant.java
+++ teach/polymorphism/Elephant.java
@@ -1,2 +1,10 @@
-package polymorphism;public class Elephant {
+package polymorphism;
+
+public class Elephant extends Animal2{
+    public void drinkWater(){
+        System.out.println("As an Elephant, I drink water");
+    }
+    public void makeSound(){
+        System.out.println("As an Elephant, I make sound");
+    }
 }
