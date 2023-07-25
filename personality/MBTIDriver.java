f44f128a2e237446b4b51049b7f4530f6d22621e
Olawalesegun <olawalesegun0@gmail.com>
2023-07-25 23:47:13 +0100

    mbti personality test

--- personality/MBTIDriver.java
+++ personality/MBTIDriver.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
+
 public class MBTIDriver {
+    public static void main(String[] args) {
+        System.out.println("Enter your name to take the test: ");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        User user = new User(name);
+        user.takeTest();
+        System.out.println("PERSONALITY TRAIT SCORE");
+        System.out.println(user.getPersonalityTrait());
+    }
 }
