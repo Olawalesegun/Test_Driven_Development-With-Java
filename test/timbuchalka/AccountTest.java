9b5d26caac7e53eb94b19bd8c3f0e3b914bbab5a
Olawalesegun <olawalesegun0@gmail.com>
2023-07-25 23:52:44 +0100

    Account test

--- test/timbuchalka/AccountTest.java
+++ test/timbuchalka/AccountTest.java
@@ -1,6 +1,24 @@
 package timbuchalka;
 
+import org.testng.annotations.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
-class AccountTest {
-  
+
+public class AccountTest {
+    Account2 acc = new Account2("0053217896", 0, "Fisayo", "fisayoGabanti", "08124567899");
+    @Test
+    public void testThatAccountCanBeCreated(){
+        assertNotNull(acc);
+    }
+    @Test
+    public void testThatAccountIsEmptyWithNoCash(){
+        acc.setAccountNumber("0053217896");
+        String accountBalance = acc.getAccountNumber();
+        double accBal = acc.checkAccountBalance(accountBalance);
+        assertEquals(0, accBal);
+    }
+    @Test
+    public void testThatAccountCanOnlyBeAccessedByActualOwner(){
+
+    }
 }
