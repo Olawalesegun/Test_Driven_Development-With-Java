7f793e25c45f5b4a1a8c34ac73a3d92ee1f0c344
Olawalesegun <olawalesegun0@gmail.com>
2023-08-10 23:17:29 +0100

    return string test

--- test/classPractice/ReturnStringTest.java
+++ test/classPractice/ReturnStringTest.java
@@ -1,6 +1,19 @@
 package classPractice;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ReturnStringTest {
-  
+
+    @Test
+    public void testThatMethodWorks(){
+        ReturnString retStr = new ReturnString();
+        String firstStringEntered = "JamBBola";
+        String secondStringEntered = "ZACTToJlma";
+        StringBuilder result = retStr.checkStringForSameValue(firstStringEntered, secondStringEntered);
+        assertEquals("JBB",result.toString());
+    }
+
+
 }
