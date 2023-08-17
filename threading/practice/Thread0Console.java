e40928f2e9fa5b1354162a1a2c697f1b16d05900
Olawalesegun <olawalesegun0@gmail.com>
2023-08-17 23:39:21 +0100

    Thread 0 console.java

--- threading/practice/Thread0Console.java
+++ threading/practice/Thread0Console.java
@@ -1,26 +1,30 @@
 package practice;
 
 public class Thread0Console {
-    public static void main(String[] args) throws InterruptedException {
-        System.out.println("Main Thread starts now");
-        Thread0 thr = new Thread0();
+    public static void main(String[] args){
+       try{
+           System.out.println("Main Thread starts now");
+           Thread0 thr = new Thread0();
 
-        Thread tr = new Thread(thr);
-        Thread tr2 = new Thread(thr);
-        Thread tr3 = new Thread(thr);
+           Thread tr = new Thread(thr);
+           Thread tr2 = new Thread(thr);
+           Thread tr3 = new Thread(thr);
 
-        tr.setName("FIRST");
-        tr2.setName("SECOND");
-        tr3.setName("THIRD");
+           tr.setName("FIRST");
+           tr2.setName("SECOND");
+           tr3.setName("THIRD");
 
-        tr.start();
-        tr2.start();
-        tr3.start();
+           tr.start();
+           tr2.start();
+           tr3.start();
 
-        tr.join();
-        tr2.join();
-        tr3.join();
+           tr.join();
+           tr2.join();
+           tr3.join();
 
-        System.out.println("Main Thread ends now!!!");
+           System.out.println("Main Thread ends now!!!");
+       }catch(InterruptedException e){
+           e.printStackTrace();
+        }
     }
 }
