8ec9bc607276af384ca8f64b9e3273a3df74adf4
Olawalesegun <olawalesegun0@gmail.com>
2023-08-17 23:39:54 +0100

    Regex Test

--- test/RegexTest.java
+++ test/RegexTest.java
@@ -1,9 +1,30 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 class RegexTest {
 
     @BeforeEach
     void setUp() {
+    }
+   /* Quantifiers
+    Character class
+    Capture groups*/
+    @Test
+    public void testRegex(){
+        String text = "AAA";
+        String pattern = "A{2, 3}";
+
+        /*String text = "AdelAjA";
+        String pattern = "[A-z]";*/
+     /*   String text = "9"
+        String pattern = "\\d"; // One only
+        String pattern = "\\d?"; // oNe opr Zero
+        String pattern = "\\d+" // One or More
+        String pattern = "\\d\\d"; Two digits
+        String pattern = "\\d{11}";*/
+        boolean result = Regex.findMatch(text, pattern);
+        assertTrue(result);
     }
 }
