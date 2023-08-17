372274286afc1c98efb59d2bdf6c63c84c0e35ea
Olawalesegun <olawalesegun0@gmail.com>
2023-08-17 01:22:36 +0100

    threads and practices

--- threading/practice/ThreadConsole.java
+++ threading/practice/ThreadConsole.java
@@ -1,4 +1,28 @@
 package practice;
 
 public class ThreadConsole {
+
+    public static void main(String[] args) {
+        try{
+            System.out.println("Main Method commence running");
+            Thread1 thr = new Thread1();
+            Thread2 thr2 = new Thread2();
+            Thread3 thr3 = new Thread3();
+            Thread tr = Thread.currentThread();
+            System.out.println(tr.getName());
+            Thread.sleep(4000);
+            System.out.println(tr.getPriority());
+
+            thr.start();
+            thr2.start();
+            thr3.start();
+            System.out.println("Main Method ends");
+           /* thr.print();
+            tr.run();*/
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
+
+
+    }
 }
