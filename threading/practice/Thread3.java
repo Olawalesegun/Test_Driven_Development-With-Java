f72c0136720c793c49c539de4fddd6f48c387eac
Olawalesegun <olawalesegun0@gmail.com>
2023-08-17 01:22:19 +0100

    threads and practices

--- threading/practice/Thread3.java
+++ threading/practice/Thread3.java
@@ -1,2 +1,16 @@
-package practice;public class Thread3 {
+package practice;
+
+public class Thread3 extends Thread{
+
+    @Override
+    public void run(){
+        show();
+    }
+    public void show(){
+        System.out.println("Third Thread runs");
+        System.out.println("======================");
+        System.out.println("No activity");
+        System.out.println("Third Thread ends");
+    }
+
 }
