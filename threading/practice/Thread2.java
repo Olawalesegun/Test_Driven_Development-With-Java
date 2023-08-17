ff9af9992dd986e15ee57952c77fdbed9297cc03
Olawalesegun <olawalesegun0@gmail.com>
2023-08-17 01:21:24 +0100

    threads and practices

--- threading/practice/Thread2.java
+++ threading/practice/Thread2.java
@@ -1,2 +1,17 @@
-package practice;public class Thread2 {
+package practice;
+
+public class Thread2 extends Thread{
+
+    @Override
+    public void run(){
+        printAlphabet();
+    }
+    public void printAlphabet(){
+        int maxCount = 70;
+        System.out.println("Second Thread runs");
+        for(int count = 65; count<maxCount; count++){
+            System.out.println((char)count);
+        }
+        System.out.println("Second Thread ends!!!");
+    }
 }
