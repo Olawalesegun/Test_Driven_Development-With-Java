6e9ea675a806907680adb854c199f43e51e6884c
Olawalesegun <olawalesegun0@gmail.com>
2023-08-17 23:38:17 +0100

    reg no ex yet

--- regex/Regex.java
+++ regex/Regex.java
@@ -1,2 +1,15 @@
+import java.util.stream.Collectors;
+
 public class Regex {
+
+    public static boolean findMatch(String text, String pattern) {
+        boolean result = false;
+        if(text.contains(pattern)){
+            result= true;
+        }
+        return result;
+    }
+
+    /*public void findMostFrequentCharacter(String input){
+        input.chars().boxed().collect(Collectors.toMap(Char*/
 }
