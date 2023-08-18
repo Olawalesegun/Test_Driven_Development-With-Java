b231ee1ec9f477e4dba18db97c64e888fbcbeb25
Olawalesegun <olawalesegun0@gmail.com>
2023-08-18 21:59:22 +0100

    File io Create file and Moving file

--- fileio/FileIOMove.java
+++ fileio/FileIOMove.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class FileIOMove {
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+public class FileIOMove {
+
+    public static void main(String[] args) {
+
+        String origin = "/home/user/Downloads/Semicolonn/sulti-wallet/NewRecord.java";
+        String destination = "/home/user/Documents";
+        Path source = Path.of(origin);
+        Path dest = Paths.get(destination);
+        try {
+            Files.move(source, dest, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println(e.getMessage());
+        }
+    }
 }
