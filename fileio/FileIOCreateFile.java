dad5f0bb77a99419d61f7d975d9ec68f970d542f
Olawalesegun <olawalesegun0@gmail.com>
2023-08-18 21:58:57 +0100

    File io Create file

--- fileio/FileIOCreateFile.java
+++ fileio/FileIOCreateFile.java
@@ -1,2 +1,18 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class FileIOCreateFile {
+
+    public static void main(String[] args) {
+        String location3 = "/home/user/Downloads/Semicolonn/sulti-wallet";
+        Path path3 = Path.of(location3);
+        try{
+            Files.createTempFile(path3, "out", ".txt");
+            //Path filePath = Files.createFile(path3);
+        }catch(IOException e){
+            e.printStackTrace();
+            System.err.println(e.getMessage());
+        }
+    }
 }
