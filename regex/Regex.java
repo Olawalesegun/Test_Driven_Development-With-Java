5725ca549b587b25d8c22022c75abf07f488f71b
Olawalesegun <olawalesegun0@gmail.com>
2023-08-18 22:01:59 +0100

    regex -------

--- regex/Regex.java
+++ regex/Regex.java
@@ -3,10 +3,18 @@ import java.util.stream.Collectors;
 public class Regex {
 
     public static boolean findMatch(String text, String pattern) {
-        boolean result = false;
+        boolean result = text.matches(pattern);
+        return result;
+       /* boolean result = false;
         if(text.contains(pattern)){
             result= true;
         }
+        return result;*/
+    }
+    public static boolean validatePhoneNumber(String phoneNumber){
+        String regex = "((^234)[0-9]{10})/((^0)\\d{10})"; // actual one to use coz it passed all test cases
+       // String regx = "^[+2340-]*?\\d{10}";
+        boolean result = phoneNumber.matches(regex);
         return result;
     }
 
