9af7752c73084e5e7597c87b5e0f76c3d735ed15
Olawalesegun <olawalesegun0@gmail.com>
2023-08-21 23:23:23 +0100

    ret inp

--- fileio/practice/ReturnInput.java
+++ fileio/practice/ReturnInput.java
@@ -1,4 +1,14 @@
 package practice;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class ReturnInput {
+
+    public static void main(String[] args) {
+        String location = "/home/Downloads";
+        Path pth = Paths.get(location);
+        System.out.println(pth.getFileSystem());
+
+    }
 }
