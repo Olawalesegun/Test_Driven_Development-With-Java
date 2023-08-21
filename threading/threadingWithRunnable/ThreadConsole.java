21bafa0dcc28dcb2a01d9e65b908837489fd939c
Olawalesegun <olawalesegun0@gmail.com>
2023-08-21 23:22:18 +0100

    threading ---

--- threading/threadingWithRunnable/ThreadConsole.java
+++ threading/threadingWithRunnable/ThreadConsole.java
@@ -1,2 +1,32 @@
-package threadingWithRunnable;public class ThreadConsole {
+package threadingWithRunnable;
+
+public class ThreadConsole {
+
+    public static void main(String[] args) {
+
+        try{
+            Thread1 thr = new Thread1();
+
+            Thread thread1 = new Thread(thr);
+            Thread thread2 = new Thread(thr);
+            Thread thread3 = new Thread(thr);
+
+            thread1.setName("April");
+            thread2.setName("May");
+            thread3.setName("June");
+            if("April".equals(thread1.getName())){
+
+            }
+
+            thread1.start();
+
+            thread2.start();
+            thread3.start();
+
+        }
+        catch(Exception e){
+            System.err.println(e.getMessage());
+        }
+
+    }
 }
