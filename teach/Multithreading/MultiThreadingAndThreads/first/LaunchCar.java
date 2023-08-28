344b086ba7a622cadbec61109f83aa1e291dc767
Olawalesegun <olawalesegun0@gmail.com>
2023-08-28 20:13:36 +0100

    teachings and practices on MultiThreading

--- teach/Multithreading/MultiThreadingAndThreads/first/LaunchCar.java
+++ teach/Multithreading/MultiThreadingAndThreads/first/LaunchCar.java
@@ -1,2 +1,23 @@
-package Multithreading.MultiThreadingAndThreads.first;public class LaunchCar {
+package Multithreading.MultiThreadingAndThreads.first;
+
+public class LaunchCar {
+    public static void main(String[] args)
+    {
+        Car c=new Car();
+
+        Thread t1=new Thread(c);
+        Thread t2=new Thread(c);
+        Thread t3=new Thread(c);
+
+        t1.setName("Child1");
+        t2.setName("Child2");
+        t3.setName("Child3");
+
+        t1.start();
+
+        t2.start();
+        t3.start();
+
+    }
+
 }
