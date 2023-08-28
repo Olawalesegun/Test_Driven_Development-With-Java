9b5f1ef2b98c02933106c58433005f0a4491f520
Olawalesegun <olawalesegun0@gmail.com>
2023-08-28 20:18:45 +0100

    teachings and practices on implementing Threads with Runnable

--- teach/Multithreading/MultiThreadingAndThreads/second/Library.java
+++ teach/Multithreading/MultiThreadingAndThreads/second/Library.java
@@ -1,2 +1,69 @@
-package Multithreading.MultiThreadingAndThreads.second;public class Library {
+package Multithreading.MultiThreadingAndThreads.second;
+
+class Library implements Runnable
+{
+    String res1=new String("JAVA");
+    String res2=new String("DSA");
+    String res3=new String("SpringBoot");
+
+    @Override
+    public void run()
+    {
+        String tname=Thread.currentThread().getName();
+        if("Student1".equals(tname))
+        {
+            try
+            {
+                Thread.sleep(4000);
+                synchronized(res1)
+                {
+                    System.out.println("Student1 has acquired : "+ res1);
+                    Thread.sleep(4000);
+                    synchronized(res2)
+                    {
+                        System.out.println("Student1 has acquired : "+ res2);
+                        Thread.sleep(4000);
+                        synchronized(res3)
+                        {
+                            System.out.println("Student1 has acquired : "+ res3);
+                        }
+
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                System.out.println("Some problem");
+            }
+        }
+        else
+        {
+            try
+            {
+                Thread.sleep(4000);
+                synchronized(res1)
+                {
+
+                    System.out.println("Student2 has acquired : "+ res1);
+                    Thread.sleep(4000);
+                    synchronized(res2)
+                    {
+                        System.out.println("Student2 has acquired : "+ res2);
+                        Thread.sleep(4000);
+                        synchronized(res3)
+                        {
+                            System.out.println("Student2 has acquired : "+ res3);
+                        }
+
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                System.out.println("Some problem");
+            }
+
+        }
+
+    }
 }
