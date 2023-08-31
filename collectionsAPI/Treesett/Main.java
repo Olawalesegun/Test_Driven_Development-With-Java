9e79c75750ae59eb9426144e4c4a9b7dd9bce0b0
Olawalesegun <olawalesegun0@gmail.com>
2023-08-31 20:38:14 +0100

    Treeset ---

--- collectionsAPI/Treesett/Main.java
+++ collectionsAPI/Treesett/Main.java
@@ -1,4 +1,19 @@
 package Treesett;
 
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 public class Main {
+
+    public static void main(String[] args) {
+        TreeSet<Integer> numbers = new TreeSet<>();
+
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+        numbers.add(40);
+
+        SortedSet<Integer> set = numbers.headSet(20); // [10]
+        System.out.println(set);
+    }
 }
