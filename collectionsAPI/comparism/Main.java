8cd1510c00a067bb42db9f0b2108203d554569fc
Olawalesegun <olawalesegun0@gmail.com>
2023-08-31 19:53:26 +0100

    Comparator -----

--- collectionsAPI/comparism/Main.java
+++ collectionsAPI/comparism/Main.java
@@ -1,4 +1,14 @@
 package comparism;
 
+import java.util.Comparator;
+
 public class Main {
+    public static void main(String[] args) {
+
+
+        Comparator<Cat> catComparator = (firstOlongbo, secondOlongbo)->{
+            if(firstOlongbo.getName().compareTo(secondOlongbo.getName())>0) return 1;
+            else if(firstOlongbo)
+        }
+    }
 }
