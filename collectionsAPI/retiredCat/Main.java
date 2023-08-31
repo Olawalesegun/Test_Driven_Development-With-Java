f5256fe73269e3377d758a970b938ed2480b6a58
Olawalesegun <olawalesegun0@gmail.com>
2023-08-31 20:02:38 +0100

    Comparator ---

--- collectionsAPI/retiredCat/Main.java
+++ collectionsAPI/retiredCat/Main.java
@@ -1,4 +1,29 @@
 package retiredCat;
 
+import comparism.Cat;
+
+import java.util.Comparator;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Comparator<Cat> catComparator = (cat1, cat2)->{
+            if(cat1.getName().compareTo(cat2.getName())>0) return 1;
+            if(cat1.getName().compareTo(cat2.getName())<0) return 1;
+            return 0;
+        };
+        Set<Cat> cats = new TreeSet<>();
+        cats.add(new Cat("Bruno", 50));
+        cats.add(new Cat("Paulty", 10));
+        cats.add(new Cat("Young-John", 20));
+        cats.add(new Cat("Shigo Baba", 50));
+
+        System.out.println(cats);   //[Bruno, Pauly, Shigo Baba], Young John]
+
+        SortedSet<Cat> catss = cats.//cats.tailSet(new Cat("Young John", 20));
+        System.out.println(catss); //[Bruno, Pauly, Shigo Baba]
+    }
 }
