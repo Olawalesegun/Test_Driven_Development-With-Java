9790af5dcc42d3dd3aa8ee7589b2f4fe0ebe2cde
Olawalesegun <olawalesegun0@gmail.com>
2023-09-06 23:18:50 +0100

    practice

--- ClassWork/ArrayQues.java
+++ ClassWork/ArrayQues.java
@@ -1,2 +1,28 @@
+import java.util.ArrayList;
+
 public class ArrayQues {
+
+    // write a method that takes in an array and returns an array double it size
+    private int[] arrayUsed;
+    private boolean validateArray;
+    public void takeInArray(int[] array1) {
+            if (validateArrayLength(array1)) {
+                arrayUsed = new int[array1.length * 2];
+                for (int count = 0; count < array1.length; count++) {
+                    arrayUsed[count] = array1[count];
+                }
+            }
+            else{
+                System.out.println("Kindly input a value with more than one character!!");
+            }
+    }
+    public int[] doubledArrayReturned(){
+        return arrayUsed;
+    }
+    public boolean validateArrayLength(int[] array){
+         validateArray = array.length > 1 ? true : false;
+         return validateArray;
+    }
+
+
 }
