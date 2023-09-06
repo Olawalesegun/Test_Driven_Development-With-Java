cad408426df967635beb2f9b22caa088964788a7
Olawalesegun <olawalesegun0@gmail.com>
2023-09-06 23:19:33 +0100

    Person.java

--- ClassWork/Person.java
+++ ClassWork/Person.java
@@ -1,2 +1,34 @@
+import enums.EducationProblem;
+import enums.FinancialProblem;
+import enums.HealthProblem;
+
 public class Person {
+
+    private FinancialProblem finance;
+    private HealthProblem health;
+    private EducationProblem edu;
+    private boolean isSolved;
+
+    public Person(FinancialProblem finance, HealthProblem health, EducationProblem edu){
+        this.finance = finance;
+        this.health = health;
+        this.edu = edu;
+    }
+
+    public String[] addProblem(){
+        System.out.println("What kind of Problem is this?: ");
+        String[] problemsForPerson = new String[3];
+        problemsForPerson.clone();
+        return problemsForPerson;
+    }
+    public Boolean solveProblem(){
+        if (isSolved = true) return true;
+        else return false;
+    }
+    public String getUnsolvedProblems(){
+        return null;
+    }
+
+    private enum Problems{problemName, problemType}
+
 }
