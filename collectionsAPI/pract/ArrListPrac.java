8acdf0d804a86bc0f5969619476684d574bbfba5
Olawalesegun <olawalesegun0@gmail.com>
2023-09-09 19:35:08 +0100

    collections API

--- collectionsAPI/pract/ArrListPrac.java
+++ collectionsAPI/pract/ArrListPrac.java
@@ -1,2 +1,27 @@
-package pract;public class ArrListPrac {
+package pract;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ArrListPrac {
+
+    public static void main(String[] args) {
+
+        List list = new ArrayList();
+        list.add(34);
+        list.add(48);
+        list.add(87);
+        list.add(67);
+        list.add(83);
+
+        Iterator iter = list.iterator();
+
+        while(iter.hasNext()){
+            System.out.println(iter.next());
+        }
+        System.out.println(list.get(0));
+
+        System.out.println(list);
+    }
 }
