ccd25774977f5594a746e5cb9dad5c962d0f23a8
Olawalesegun <olawalesegun0@gmail.com>
2023-09-10 23:44:10 +0100

    Using the comparator interface

--- collectionsAPI/pract/comparator/ComparatorConsole.java
+++ collectionsAPI/pract/comparator/ComparatorConsole.java
@@ -1,4 +1,14 @@
 package pract.comparator;
 
-public class ComparatorConsole {
+import java.util.Comparator;
+
+public class ComparatorConsole implements Comparator<ComparatorClass> {
+    @Override
+    public int compare(ComparatorClass cc1, ComparatorClass cc2) {
+        if(cc1.getId() > cc2.getAge()){
+            return 1;
+        }else{
+            return -1;
+        }
+    }
 }
