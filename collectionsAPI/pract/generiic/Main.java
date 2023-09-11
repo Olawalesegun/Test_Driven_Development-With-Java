05e206c5c5f1f2df9842c0487d9b5a2bfdd3694a
Olawalesegun <olawalesegun0@gmail.com>
2023-09-11 22:57:19 +0100

    hands on generic

--- collectionsAPI/pract/generiic/Main.java
+++ collectionsAPI/pract/generiic/Main.java
@@ -1,2 +1,23 @@
-package pract.generiic;public class Main {
+package pract.generiic;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Agent<Integer> agent = new Agent();
+        agent.setId(1);
+        //agent.setName(Integer.valueOf());
+
+        System.out.println(String.valueOf(456));
+
+        System.out.println(agent.getName());
+
+
+        System.out.println((int) '0');
+
+        System.out.println(Integer.parseInt("5675"));
+
+        System.out.println(Integer.parseInt("5675"));
+
+        System.out.println(Integer.parseInt("Police"));
+    }
 }
