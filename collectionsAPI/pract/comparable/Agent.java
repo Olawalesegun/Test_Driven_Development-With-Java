ae14a0c64109d5cce2435d19932feddb6ccfe00e
Olawalesegun <olawalesegun0@gmail.com>
2023-09-12 22:53:13 +0100

    implementing comparable for collections

--- collectionsAPI/pract/comparable/Agent.java
+++ collectionsAPI/pract/comparable/Agent.java
@@ -2,14 +2,36 @@ package pract.comparable;
 
 import org.jetbrains.annotations.NotNull;
 
-public class Agent implements Comparable{
+public class Agent implements Comparable<Agent>{
 
     private int id;
     private String name;
     private String email;
+    int age;
+
+    public Agent(int id, String name, String email, int age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
 
     @Override
-    public int compareTo(@NotNull Object o) {
-        return 0;
+    public int compareTo(@NotNull Agent o) {
+        if(this.age>o.age){
+            return 1;
+        }else{
+            return -1;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Agent{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
---
+++ collectionsAPI/pract/hash/HashMapp.java
@@ -0,0 +1,8 @@
+package pract.hash;
+
+public class HashMap {
+
+    public static void main(String[] args) {
+        HashMap hashMap = new java.util.HashMap<>()
+    }
+}
