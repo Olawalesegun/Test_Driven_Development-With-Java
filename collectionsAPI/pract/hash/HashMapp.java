f909e4b9c1bbd99919339cc2a7fd9602c6587f75
Olawalesegun <olawalesegun0@gmail.com>
2023-09-12 22:55:18 +0100

    implementing hashmap and understanding wjat makes it special

--- collectionsAPI/pract/hash/HashMapp.java
+++ collectionsAPI/pract/hash/HashMapp.java
@@ -1,8 +1,35 @@
 package pract.hash;
 
-public class HashMap {
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.TreeMap;
+
+public class HashMapp {
 
     public static void main(String[] args) {
-        HashMap hashMap = new java.util.HashMap<>()
+        HashMap hashMap = new HashMap();
+        hashMap.put(2,24);
+        hashMap.put(1, 22);
+        hashMap.put("null", 56);
+
+        System.out.println(hashMap);
+
+        LinkedHashMap linkedHashMap = new LinkedHashMap();
+        linkedHashMap.put(2, 24);
+        linkedHashMap.put(1, 22);
+        linkedHashMap.put(0, 19);
+        linkedHashMap.put(3, 34);
+
+        System.out.println(linkedHashMap);
+
+        TreeMap treeMap = new TreeMap();
+
+        treeMap.put(4, 45);
+        treeMap.put(1, 22);
+        treeMap.put(3, 456);
+        treeMap.put(5, 23);
+
+        System.out.println(treeMap);
+
     }
 }
