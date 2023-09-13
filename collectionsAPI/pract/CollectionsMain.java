890453244406764d71a99792fde3d913096683c1
Olawalesegun <olawalesegun0@gmail.com>
2023-09-13 22:55:33 +0100

    sorting

--- collectionsAPI/pract/CollectionsMain.java
+++ collectionsAPI/pract/CollectionsMain.java
@@ -1,10 +1,24 @@
 package pract;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
-public class Main {
+public class CollectionsMain {
 
     public static void main(String[] args) {
-        CollectionsAndSortingWithComparator coll = new CollectionsAndSortingWithComparator();
+        CollectionsAndSortingWithComparator coll = new CollectionsAndSortingWithComparator(5);
+        CollectionsAndSortingWithComparator coll1 = new CollectionsAndSortingWithComparator(7);
+        CollectionsAndSortingWithComparator coll2 = new CollectionsAndSortingWithComparator(8);
+
+        CollectionsComparatorConsole colComConsole = new CollectionsComparatorConsole();
+
+        ArrayList arrList = new ArrayList();
+        arrList.add(coll);
+        arrList.add(coll1);
+        arrList.add(coll2);
+
+        Collections.sort(arrList, colComConsole);
+
+        System.out.println(arrList);
     }
 }
