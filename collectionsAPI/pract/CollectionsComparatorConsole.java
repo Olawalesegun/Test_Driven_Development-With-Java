3599779b0ff894ca3e324adeb92280037437ce56
Olawalesegun <olawalesegun0@gmail.com>
2023-09-13 22:55:11 +0100

    sorting in collections with comparator

--- collectionsAPI/pract/CollectionsComparatorConsole.java
+++ collectionsAPI/pract/CollectionsComparatorConsole.java
@@ -1,4 +1,15 @@
 package pract;
 
-public class CollectionsComparatorConsole {
+import java.util.Comparator;
+
+public class CollectionsComparatorConsole implements Comparator<CollectionsAndSortingWithComparator> {
+    @Override
+    public int compare(CollectionsAndSortingWithComparator t1,
+                       CollectionsAndSortingWithComparator t2) {
+        if(t1.getAge()>t2.getAge()){
+            return 1;
+        }else{
+            return -1;
+        }
+    }
 }
