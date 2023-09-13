9689041d705ee23c62099446345ddf8215418223
Olawalesegun <olawalesegun0@gmail.com>
2023-09-13 22:54:00 +0100

    sorting in collections

--- collectionsAPI/pract/CollectionsAndSorting.java
+++ collectionsAPI/pract/CollectionsAndSorting.java
@@ -1,4 +1,25 @@
 package pract;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class CollectionsAndSorting {
+
+    public static void main(String[] args) {
+
+        ArrayList arrayList = new ArrayList();
+        arrayList.add(345);
+        arrayList.add(675);
+        arrayList.add(744);
+        arrayList.add(898);
+        arrayList.add(56);
+
+        System.out.println("The arrayList before being sorted: " + arrayList);
+
+        Collections.sort(arrayList);
+
+        System.out.println("The arrayList after being sorted: " + arrayList);
+
+
+    }
 }
